691275aa0db976d511828f6ec9d9c7ada9598ab9
dennyverbeeck <dennyverbeeck@gmail.com>
2017-06-22 10:37:19 +0200

    Add roles claim to JWT

--- src/main/java/org/radarcns/management/security/ClaimsTokenEnhancer.java
+++ src/main/java/org/radarcns/management/security/ClaimsTokenEnhancer.java
@@ -1,12 +1,10 @@
 package org.radarcns.management.security;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import org.radarcns.management.domain.Role;
 import org.radarcns.management.domain.Source;
+import org.radarcns.management.domain.User;
 import org.radarcns.management.repository.SubjectRepository;
+import org.radarcns.management.repository.UserRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -16,10 +14,21 @@ import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class ClaimsTokenEnhancer implements TokenEnhancer, InitializingBean {
 
     @Autowired
     private SubjectRepository subjectRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken,
@@ -27,15 +36,22 @@ public class ClaimsTokenEnhancer implements TokenEnhancer, InitializingBean {
         if (authentication.getAuthorities() != null && !authentication.getAuthorities().isEmpty()) {
             Map<String, Object> additionalInfo = new HashMap<>();
             Set<String> currentUserAuthorities = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-//            additionalInfo.put("authorities" , currentUserAuthorities);
+
+            String userName = null;
+            if (authentication.getPrincipal() instanceof UserDetails) {
+                UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+                userName = springSecurityUser.getUsername();
+            } else if (authentication.getPrincipal() instanceof String) {
+                userName = (String) authentication.getPrincipal();
+            }
+
+            Optional<User> optUser = userRepository.findOneByLogin(userName);
+            if (optUser.isPresent()) {
+                List<Role> roles = optUser.get().getRoles().stream().collect(Collectors.toList());
+                additionalInfo.put("roles", roles);
+            }
+
             if(currentUserAuthorities.contains(AuthoritiesConstants.PARTICIPANT)) {
-                String userName = null;
-                if (authentication.getPrincipal() instanceof UserDetails) {
-                    UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
-                    userName = springSecurityUser.getUsername();
-                } else if (authentication.getPrincipal() instanceof String) {
-                    userName = (String) authentication.getPrincipal();
-                }
                 if(userName!=null) {
                     List<Source> assignedSources = subjectRepository
                         .findSourcesBySubjectLogin(userName);
