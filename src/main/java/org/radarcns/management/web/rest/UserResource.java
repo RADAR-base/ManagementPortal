935ceef0f90fab766a5efb4c0d90f81a6aced08d
dennyverbeeck <dennyverbeeck@gmail.com>
2017-08-09 13:27:03 +0200

    Revert UserResource to commit d3d9fe
    This change removes the /api/users/me endpoint, we already have an endpoint to get current user details at /api/account

--- src/main/java/org/radarcns/management/web/rest/UserResource.java
+++ src/main/java/org/radarcns/management/web/rest/UserResource.java
@@ -1,18 +1,19 @@
 package org.radarcns.management.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
-import io.github.jhipster.web.util.ResponseUtil;
-import io.swagger.annotations.ApiParam;
 import org.radarcns.management.config.Constants;
+import com.codahale.metrics.annotation.Timed;
 import org.radarcns.management.domain.User;
 import org.radarcns.management.repository.UserRepository;
 import org.radarcns.management.security.AuthoritiesConstants;
 import org.radarcns.management.service.MailService;
 import org.radarcns.management.service.UserService;
 import org.radarcns.management.service.dto.UserDTO;
+import org.radarcns.management.web.rest.vm.ManagedUserVM;
 import org.radarcns.management.web.rest.util.HeaderUtil;
 import org.radarcns.management.web.rest.util.PaginationUtil;
-import org.radarcns.management.web.rest.vm.ManagedUserVM;
+import io.github.jhipster.web.util.ResponseUtil;
+import io.swagger.annotations.ApiParam;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -21,21 +22,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * REST controller for managing users.
@@ -196,18 +187,5 @@ public class UserResource {
         log.debug("REST request to delete User: {}", login);
         userService.deleteUser(login);
         return ResponseEntity.ok().headers(HeaderUtil.createAlert( "userManagement.deleted", login)).build();
-    }
-
-    @GetMapping("/users/me")
-    @Timed
-    public ResponseEntity<UserDTO> getMe(Authentication authentication) {
-        String userName = null;
-        if (authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
-            userName = springSecurityUser.getUsername();
-        } else if (authentication.getPrincipal() instanceof String) {
-            userName = (String) authentication.getPrincipal();
-        }
-        return ResponseUtil.wrapOrNotFound(userService.getUserWithAuthoritiesByLogin(userName));
     }
 }
