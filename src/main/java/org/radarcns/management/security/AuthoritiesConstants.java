18321d4e6f1fc824b8759ce8f4deb15579d449eb
nivethika <nivemaham@gmail.com>
2017-09-18 11:20:59 +0200

    small optimizations

--- src/main/java/org/radarcns/management/security/AuthoritiesConstants.java
+++ src/main/java/org/radarcns/management/security/AuthoritiesConstants.java
@@ -3,30 +3,26 @@ package org.radarcns.management.security;
 /**
  * Constants for Spring Security authorities.
  */
-public final class AuthoritiesConstants {
+public interface AuthoritiesConstants {
 
-//    public static final String ADMIN = "ROLE_ADMIN";
+    String SYS_ADMIN = "ROLE_SYS_ADMIN";
 
-    public static final String SYS_ADMIN = "ROLE_SYS_ADMIN";
+    String PROJECT_ADMIN = "ROLE_PROJECT_ADMIN";
 
-    public static final String PROJECT_ADMIN = "ROLE_PROJECT_ADMIN";
+    String PROJECT_OWNER = "ROLE_PROJECT_OWNER";
 
-    public static final String PROJECT_OWNER = "ROLE_PROJECT_OWNER";
+    String PROJECT_AFFILIATE = "ROLE_PROJECT_AFFILIATE";
 
-    public static final String PROJECT_AFFILIATE = "ROLE_PROJECT_AFFILIATE";
+    String PROJECT_ANALYST = "ROLE_PROJECT_ANALYST";
 
-    public static final String PROJECT_ANALYST = "ROLE_PROJECT_ANALYST";
+    String USER = "ROLE_USER";
 
-    public static final String USER = "ROLE_USER";
+    String ANONYMOUS = "ROLE_ANONYMOUS";
 
-    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+    String PARTICIPANT = "ROLE_PARTICIPANT";
 
-    public static final String PARTICIPANT = "ROLE_PARTICIPANT";
+    String[] PROJECT_RELATED_AUTHORITIES = {PROJECT_ADMIN, PROJECT_OWNER, PROJECT_AFFILIATE,
+        PROJECT_ANALYST};
 
-    public static final String[] PROJECT_RELATED_AUTHORITIES = {PROJECT_ADMIN, PROJECT_OWNER , PROJECT_AFFILIATE , PROJECT_ANALYST};
-
-    public static final String EXTERNAL_ERF_INTEGRATOR = "ROLE_EXTERNAL_ERF_INTEGRATOR";
-
-    private AuthoritiesConstants() {
-    }
+    String EXTERNAL_ERF_INTEGRATOR = "ROLE_EXTERNAL_ERF_INTEGRATOR";
 }
