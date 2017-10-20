58a0fa61ec82d054adb250e24c0001e1583c6d01
dennyverbeeck <dennyverbeeck@gmail.com>
2017-10-20 15:59:32 +0200

    Add null checks

--- src/main/java/org/radarcns/management/service/mapper/ClientDetailsMapperImpl.java
+++ src/main/java/org/radarcns/management/service/mapper/ClientDetailsMapperImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,17 +25,25 @@ public class ClientDetailsMapperImpl implements ClientDetailsMapper {
     public ClientDetailsDTO clientDetailsToClientDetailsDTO(ClientDetails details) {
         ClientDetailsDTO result = new ClientDetailsDTO();
         result.setClientId(details.getClientId());
-        result.setScope(Collections.unmodifiableSet(details.getScope()));
-        result.setResourceIds(Collections.unmodifiableSet(details.getResourceIds()));
-        result.setAuthorizedGrantTypes(Collections.unmodifiableSet(details.getAuthorizedGrantTypes()));
-        result.setAccessTokenValidity(details.getAccessTokenValiditySeconds().longValue());
-        result.setRefreshTokenValidity(details.getRefreshTokenValiditySeconds().longValue());
-        Set<String> authorities = details.getAuthorities().stream()
-            .map(GrantedAuthority::getAuthority)
-            .collect(Collectors.toSet());
-        result.setAuthorities(Collections.unmodifiableSet(authorities));
-        result.setAdditionalInformation(details.getAdditionalInformation().entrySet().stream()
-            .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().toString())));
+        result.setScope(details.getScope());
+        result.setResourceIds(details.getResourceIds());
+        result.setAuthorizedGrantTypes(details.getAuthorizedGrantTypes());
+        if (Objects.nonNull(details.getAccessTokenValiditySeconds())) {
+            result.setAccessTokenValidity(details.getAccessTokenValiditySeconds().longValue());
+        }
+        if (Objects.nonNull(details.getRefreshTokenValiditySeconds())) {
+            result.setRefreshTokenValidity(details.getRefreshTokenValiditySeconds().longValue());
+        }
+        if (Objects.nonNull(details.getAuthorities())) {
+            Set<String> authorities =
+                details.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
+            result.setAuthorities(authorities);
+        }
+        if (Objects.nonNull(details.getAdditionalInformation())) {
+            result.setAdditionalInformation(details.getAdditionalInformation().entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> Objects.nonNull(e.getValue()) ?
+                    e.getValue().toString() : null)));
+        }
         return result;
     }
 
@@ -43,31 +52,41 @@ public class ClientDetailsMapperImpl implements ClientDetailsMapper {
         BaseClientDetails result = new BaseClientDetails();
         result.setClientId(detailsDTO.getClientId());
         result.setClientSecret(detailsDTO.getClientSecret());
-        result.setScope(Collections.unmodifiableSet(detailsDTO.getScope()));
-        result.setResourceIds(Collections.unmodifiableSet(detailsDTO.getResourceIds()));
-        result.setAuthorizedGrantTypes(Collections.unmodifiableSet(detailsDTO.getAuthorizedGrantTypes()));
-        result.setAccessTokenValiditySeconds(detailsDTO.getAccessTokenValidity().intValue());
-        result.setRefreshTokenValiditySeconds(detailsDTO.getRefreshTokenValidity().intValue());
-        Set<GrantedAuthority> authorities = detailsDTO.getAuthorities().stream()
-            .map(SimpleGrantedAuthority::new)
-            .collect(Collectors.toSet());
-        result.setAuthorities(Collections.unmodifiableSet(authorities));
-        if (detailsDTO.getAutoApprove()) {
-            result.setAutoApproveScopes(Collections.unmodifiableSet(detailsDTO.getScope()));
+        result.setScope(detailsDTO.getScope());
+        result.setResourceIds(detailsDTO.getResourceIds());
+        result.setAuthorizedGrantTypes(detailsDTO.getAuthorizedGrantTypes());
+        if (Objects.nonNull(detailsDTO.getAccessTokenValidity())) {
+            result.setAccessTokenValiditySeconds(detailsDTO.getAccessTokenValidity().intValue());
         }
-        result.setAdditionalInformation(Collections.unmodifiableMap(detailsDTO
-            .getAdditionalInformation()));
+        if (Objects.nonNull(detailsDTO.getRefreshTokenValidity())) {
+            result.setRefreshTokenValiditySeconds(detailsDTO.getRefreshTokenValidity().intValue());
+        }
+        if (Objects.nonNull(detailsDTO.getAuthorities())) {
+            Set<GrantedAuthority> authorities =
+                detailsDTO.getAuthorities().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toSet());
+            result.setAuthorities(authorities);
+        }
+        if (detailsDTO.getAutoApprove()) {
+            result.setAutoApproveScopes(detailsDTO.getScope());
+        }
+        result.setAdditionalInformation(detailsDTO.getAdditionalInformation());
         return result;
     }
 
     @Override
     public List<ClientDetailsDTO> clientDetailsToClientDetailsDTO(List<ClientDetails> detailsList) {
+        if (Objects.isNull(detailsList)) {
+            return null;
+        }
         return detailsList.stream().map(this::clientDetailsToClientDetailsDTO)
             .collect(Collectors.toList());
     }
 
     @Override
     public List<ClientDetails> clientDetailsDTOToClientDetails(List<ClientDetailsDTO> detailsDTOList) {
+        if (Objects.isNull(detailsDTOList)) {
+            return null;
+        }
         return detailsDTOList.stream().map(this::clientDetailsDTOToClientDetails)
             .collect(Collectors.toList());
     }
