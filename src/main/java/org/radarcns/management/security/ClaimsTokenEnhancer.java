5fc3254062ff1f1fd5a3883c03d5d5cba1bc6a27
Denny Verbeeck <dennyverbeeck@gmail.com>
2017-11-16 17:41:43 +0100

    Add audit for successful token grant

--- src/main/java/org/radarcns/management/security/ClaimsTokenEnhancer.java
+++ src/main/java/org/radarcns/management/security/ClaimsTokenEnhancer.java
@@ -1,18 +1,19 @@
 package org.radarcns.management.security;
 
 import org.radarcns.management.domain.Source;
-import org.radarcns.management.domain.Subject;
 import org.radarcns.management.domain.User;
 import org.radarcns.management.repository.SubjectRepository;
 import org.radarcns.management.repository.UserRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.boot.actuate.audit.AuditEvent;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 
 import java.time.Instant;
@@ -21,7 +22,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ClaimsTokenEnhancer implements TokenEnhancer, InitializingBean {
@@ -32,8 +32,13 @@ public class ClaimsTokenEnhancer implements TokenEnhancer, InitializingBean {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AuditEventRepository auditEventRepository;
+
     @Value("${spring.application.name}")
     private String appName;
+
+    private final static String GRANT_TOKEN_EVENT = "GRANT_ACCESS_TOKEN";
 
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken,
@@ -50,19 +55,19 @@ public class ClaimsTokenEnhancer implements TokenEnhancer, InitializingBean {
             Optional<User> optUser = userRepository.findOneByLogin(userName);
             if (optUser.isPresent()) {
                 List<String> roles = optUser.get().getRoles().stream()
-                    .filter(role -> Objects.nonNull(role.getProject()))
-                    .map(role -> role.getProject().getProjectName() + ":"
-                        + role.getAuthority().getName())
-                    .collect(Collectors.toList());
+                        .filter(role -> Objects.nonNull(role.getProject()))
+                        .map(role -> role.getProject().getProjectName() + ":"
+                                + role.getAuthority().getName())
+                        .collect(Collectors.toList());
                 additionalInfo.put("roles", roles);
 
             }
 
-            List<Source> assignedSources = subjectRepository
-                .findSourcesBySubjectLogin(userName);
+            List<Source> assignedSources = subjectRepository.findSourcesBySubjectLogin(userName);
 
             List<String> sourceIds = assignedSources.stream()
-                .map(s -> s.getSourceId().toString()).collect(Collectors.toList());
+                    .map(s -> s.getSourceId().toString())
+                    .collect(Collectors.toList());
             additionalInfo.put("sources", sourceIds);
         }
         // add iat and iss optional JWT claims
@@ -71,10 +76,34 @@ public class ClaimsTokenEnhancer implements TokenEnhancer, InitializingBean {
         ((DefaultOAuth2AccessToken) accessToken)
             .setAdditionalInformation(additionalInfo);
 
+        // HACK: since all granted tokens need to pass here, we can use this point to create an
+        // audit event for a granted token, there is an open issue but inactive for a long time
+        // already: https://github.com/spring-projects/spring-security-oauth/issues/223
+        auditEventRepository.add(new AuditEvent(userName, GRANT_TOKEN_EVENT,
+                auditData(accessToken, authentication)));
+
         return accessToken;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
+    }
+
+    private Map<String, Object> auditData(OAuth2AccessToken accessToken,
+            OAuth2Authentication authentication) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("tokenType", accessToken.getTokenType());
+        result.put("scope", String.join(", ", accessToken.getScope()));
+        result.put("expiresIn", Integer.toString(accessToken.getExpiresIn()));
+        result.putAll(accessToken.getAdditionalInformation());
+        OAuth2Request request = authentication.getOAuth2Request();
+        result.put("clientId", request.getClientId());
+        result.put("grantType", request.getGrantType());
+        if (authentication.getDetails() instanceof OAuth2AuthenticationDetails) {
+            OAuth2AuthenticationDetails details =
+                    (OAuth2AuthenticationDetails) authentication.getDetails();
+            result.put("remoteAddress", details.getRemoteAddress());
+        }
+        return result;
     }
 }
