de15feae561f5615fef4b40a6615efcc16c03457
Denny Verbeeck <dennyverbeeck@gmail.com>
2017-11-17 14:08:42 +0100

    Add mock user to tests
    We need to mock a user in the SecurityContext, the auditor for Entity audits can not be null.

--- src/test/java/org/radarcns/management/web/rest/SubjectResourceIntTest.java
+++ src/test/java/org/radarcns/management/web/rest/SubjectResourceIntTest.java
@@ -23,6 +23,7 @@ import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mock.web.MockFilterConfig;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
@@ -54,6 +55,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ManagementPortalApp.class)
+@WithMockUser
 public class SubjectResourceIntTest {
 
     private static final String DEFAULT_EXTERNAL_LINK = "AAAAAAAAAA";
