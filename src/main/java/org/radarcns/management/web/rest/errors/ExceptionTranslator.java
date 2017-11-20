c4ae9584af0f667b1ec1ff04922c1419b46d8d31
Denny Verbeeck <dennyverbeeck@gmail.com>
2017-11-20 11:22:30 +0100

    Add exception translator for unsupported media type

--- src/main/java/org/radarcns/management/web/rest/errors/ExceptionTranslator.java
+++ src/main/java/org/radarcns/management/web/rest/errors/ExceptionTranslator.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -97,6 +98,13 @@ public class ExceptionTranslator {
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public ErrorVM processMethodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
         return new ErrorVM(ErrorConstants.ERR_METHOD_NOT_SUPPORTED, exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    public ErrorVM processMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException ex) {
+        return new ErrorVM(ErrorConstants.ERR_MEDIA_TYPE_NOT_SUPPORTED, ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
