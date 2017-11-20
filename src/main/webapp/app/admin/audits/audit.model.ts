13462dcabc4021e54ab973138a753cb14912acdf
Denny Verbeeck <dennyverbeeck@gmail.com>
2017-11-20 16:34:32 +0100

    Remove AuditData and use Map<string, string> instead

--- src/main/webapp/app/admin/audits/audit.model.ts
+++ src/main/webapp/app/admin/audits/audit.model.ts
@@ -1,6 +1,6 @@
 export class Audit {
     constructor(
-        public data: any,
+        public data: Map<string, string>,
         public principal: string,
         public timestamp: string,
         public type: string
