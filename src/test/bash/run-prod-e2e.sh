41bf31de0d7bc61f71af9817a242498192769083
Denny Verbeeck <dennyverbeeck@gmail.com>
2017-11-27 22:09:35 +0100

    Update run e2e script

--- src/test/bash/run-prod-e2e.sh
+++ src/test/bash/run-prod-e2e.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 # For testing production mode e2e tests we need some tweaks:
-# 1) The context path is at /managementportal so we need to modify protractor.conf.js
-# 2) The angular app still needs to run in dev mode for protractor to work
+# 1) The angular app needs to run in dev mode for protractor to work
+# 2) We update the basehref of the angular app to the context path of the backend
+# 3) We update the protractor configuration to the new path
 #
 # Then we start up a docker stack with a postgres server since production mode is configured for a
 # postgres database instead of in-memory database.
 
 set -ev
 
-sed -i 's|http://localhost:8080/|http://localhost:8080/managementportal/|' src/test/javascript/protractor.conf.js
-sed -i "s/DEBUG_INFO_ENABLED: options.env === 'dev'/DEBUG_INFO_ENABLED: true/" webpack/webpack.common.js
-grep baseUrl src/test/javascript/protractor.conf.js
-grep DEBUG webpack/webpack.common.js
+sed -i "s|new plugin.BaseHrefWebpackPlugin({ baseHref: '/' })|new plugin.BaseHrefWebpackPlugin({ baseHref: '/managementportal/' })|" webpack/webpack.dev.js
+sed -i "s|baseUrl: 'http://localhost:8080/',|baseUrl: 'http://localhost:8080/managementportal/',|" src/test/javascript/protractor.conf.js
+./gradlew bootRepackage -Pprod buildDocker -x test
 docker-compose -f src/main/docker/app.yml up -d # spin up production mode application
-sleep 120 # wait for app to start up
+# wait for app to start up
+echo -n "Waiting for application startup"
+until curl -s http://localhost:8080/managementportal/ > /dev/null
+do
+  echo -n "." # waiting
+  sleep 2
+done
+echo ""
+echo "Application is up"
 docker-compose -f src/main/docker/app.yml logs # show output of app startup
 yarn e2e # run e2e tests against production mode
 docker-compose -f src/main/docker/app.yml down -v # clean up containers and volumes
-git checkout src/test/javascript/protractor.conf.js  # revert protractor configuration
-git checkout webpack/webpack.common.js
+git checkout src/test/javascript/protractor.conf.js
+git checkout webpack/webpack.prod.js
