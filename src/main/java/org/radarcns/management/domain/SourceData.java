43ef79b1c65cd3fb0eeb274a61ceffc102c0607f
nivethika <nivemaham@gmail.com>
2017-11-28 11:53:51 +0100

    Updated source data views

--- src/main/java/org/radarcns/management/domain/SourceData.java
+++ src/main/java/org/radarcns/management/domain/SourceData.java
@@ -171,6 +171,12 @@ public class SourceData extends AbstractAuditingEntity implements Serializable {
         this.unit = unit;
     }
 
+    public SourceData unit(String unit) {
+        this.unit = unit;
+        return this;
+    }
+
+
     public String getDataClass() {
         return dataClass;
     }
@@ -187,12 +193,22 @@ public class SourceData extends AbstractAuditingEntity implements Serializable {
         this.valueSchema = valueSchema;
     }
 
+    public SourceData valueSchema(String valueSchema) {
+        this.valueSchema = valueSchema;
+        return this;
+    }
+
     public String getTopic() {
         return topic;
     }
 
     public void setTopic(String topic) {
         this.topic = topic;
+    }
+
+    public SourceData topic(String topic) {
+        this.topic = topic;
+        return this;
     }
 
     public String getProvider() {
@@ -210,7 +226,6 @@ public class SourceData extends AbstractAuditingEntity implements Serializable {
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
     }
-
 
     public String getSourceDataName() {
         return sourceDataName;
