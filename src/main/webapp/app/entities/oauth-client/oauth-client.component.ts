0832e5ed8ed5ee95b33d8d08e8c74d1ec77890f4
nivethika <nivemaham@gmail.com>
2017-12-11 14:33:13 +0100

    convert seconds to minutes, hours and days when applicable

--- src/main/webapp/app/entities/oauth-client/oauth-client.component.ts
+++ src/main/webapp/app/entities/oauth-client/oauth-client.component.ts
@@ -19,6 +19,13 @@ export class OAuthClientComponent implements OnInit, OnDestroy {
     eventSubscriber: Subscription;
     objectKeys = Object.keys;
 
+    times = {
+        day: 86400,
+        hour: 3600,
+        minute: 60,
+        second: 1
+    }
+
     constructor(
         private jhiLanguageService: JhiLanguageService,
         private oauthClientService: OAuthClientService,
@@ -59,5 +66,25 @@ export class OAuthClientComponent implements OnInit, OnDestroy {
 
     private onError(error) {
         this.alertService.error(error.message, null, null);
+    }
+
+    private convertSeconds(seconds: number) {
+        let time_string: string = '';
+        let plural: string = '';
+        for(var key in this.times){
+            if(Math.floor(seconds / this.times[key]) > 0){
+                if(Math.floor(seconds / this.times[key]) >1 ){
+                    plural = 's';
+                }
+                else{
+                    plural = '';
+                }
+
+                time_string += Math.floor(seconds / this.times[key]).toString() + ' ' + key.toString() + plural + ' ';
+                seconds = seconds - this.times[key] * Math.floor(seconds / this.times[key]);
+
+            }
+        }
+        return time_string;
     }
 }
