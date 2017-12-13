c4aeb16c7fab56d4cb319498bd8f056d0980fffc
Denny Verbeeck <dennyverbeeck@gmail.com>
2017-12-13 13:50:16 +0100

    Add e2e tests for create/update/delete oauth client

--- src/test/javascript/e2e/entities/oauth-client.spec.ts
+++ src/test/javascript/e2e/entities/oauth-client.spec.ts
@@ -49,6 +49,54 @@ describe('Project e2e test', () => {
             expect(count).toEqual(2));// show more, show less buttons are enabled
     });
 
+    it('should be able to create OAuth Client', () => {
+        element(by.css('jhi-oauth-client h4 button.btn-primary')).click().then(() => {
+            element(by.id('id')).sendKeys('test-client');
+            element(by.cssContainingText('button', 'Random')).click().then(() => {
+                // check if there is something put in the secret field
+                element(by.id('secret')).getAttribute('value').then((value) => expect(value.length).toBeGreaterThan(0));
+            });
+            element(by.id('scope')).sendKeys('SUBJECT.READ');
+            element(by.id('resourceIds')).sendKeys('res_ManagementPortal');
+            element(by.cssContainingText('label.form-check-label', 'refresh_token')).element(by.css('input')).click();
+            element(by.cssContainingText('label.form-check-label', 'password')).element(by.css('input')).click();
+            element(by.id('accessTokenValidity')).clear();
+            element(by.id('accessTokenValidity')).sendKeys('3600');
+            element(by.id('refreshTokenValidity')).clear();
+            element(by.id('refreshTokenValidity')).sendKeys('7200');
+            element(by.cssContainingText('button.btn-primary', 'Save')).click().then(() =>{
+                // there should be a notification popup
+                element(by.css('.alert-success')).element(by.css('pre')).getText().then((value) => {
+                    expect(value).toMatch(/A new OAuth client is created with client id test-client/);
+                });
+            });
+        });
+    });
+
+    it('should be able to edit OAuth Client', () => {
+        element(by.cssContainingText('td', 'test-client')).element(by.xpath('ancestor::tr'))
+                .element(by.cssContainingText('button', 'Edit')).click().then(() => {
+            element(by.cssContainingText('button.btn-primary', 'Save')).click().then(() =>{
+                // there should be a notification popup
+                element(by.css('.alert-success')).element(by.css('pre')).getText().then((value) => {
+                    expect(value).toMatch(/An OAuth client is updated with client id test-client/);
+                });
+            });
+        });
+    });
+
+    it('should be able to delete OAuth Client', () => {
+        element(by.cssContainingText('td', 'test-client')).element(by.xpath('ancestor::tr'))
+                .element(by.cssContainingText('button', 'Delete')).click().then(() => {
+            element(by.cssContainingText('jhi-oauth-client-delete-dialog button.btn-danger', 'Delete')).click().then(() =>{
+                // there should be a notification popup
+                element(by.css('.alert-success')).element(by.css('pre')).getText().then((value) => {
+                    expect(value).toMatch(/An OAuth client is deleted with client id test-client/);
+                });
+            });
+        });
+    });
+
     afterAll(function () {
         accountMenu.click();
         logout.click();
