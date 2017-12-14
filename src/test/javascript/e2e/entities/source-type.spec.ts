7d4635132afd80b97fe621615f7f2d5b1d4a2be3
Denny Verbeeck <dennyverbeeck@gmail.com>
2017-12-14 14:33:08 +0100

    Add e2e tests for create/update/delete SourceType

--- src/test/javascript/e2e/entities/source-type.spec.ts
+++ src/test/javascript/e2e/entities/source-type.spec.ts
@@ -42,6 +42,50 @@ describe('SourceType e2e test', () => {
         });
     });
 
+    it('should be able to create SourceType', function () {
+        element(by.css('button.create-source-type')).click().then(() => {
+            element(by.id('field_producer')).sendKeys('test-producer');
+            element(by.id('field_model')).sendKeys('test-model');
+            element(by.id('field_catalogVersion')).sendKeys('v1');
+            // select first option in the source type scope dropdown
+            element(by.id('field_sourceTypeScope')).all(by.tagName('option')).then(function(options){
+              options[0].click();
+            });
+            element(by.css('jhi-source-type-dialog')).element(by.buttonText('Save')).click().then(() => {
+                // there should be a notification popup
+                element(by.css('.alert-success')).element(by.css('pre')).getText().then((value) => {
+                    expect(value).toMatch(/A new Source Type is created with identifier test-producer test-model v1/);
+                });
+            });
+        });
+    });
+    
+    it('should be able to edit SourceType', () => {
+        element(by.cssContainingText('td', 'test-producer')).element(by.xpath('ancestor::tr'))
+                .element(by.cssContainingText('button', 'Edit')).click().then(() => {
+            browser.waitForAngular();
+            element(by.cssContainingText('button.btn-primary', 'Save')).click().then(() => {
+                // there should be a notification popup
+                element(by.css('.alert-success')).element(by.css('pre')).getText().then((value) => {
+                    expect(value).toMatch(/A Source Type is updated with identifier test-producer test-model v1/);
+                });
+            });
+        });
+    });
+
+    it('should be able to delete SourceType', () => {
+        element(by.cssContainingText('td', 'test-producer')).element(by.xpath('ancestor::tr'))
+                .element(by.cssContainingText('button', 'Delete')).click().then(() => {
+            browser.waitForAngular();
+            element(by.cssContainingText('jhi-source-type-delete-dialog button.btn-danger', 'Delete')).click().then(() =>{
+                // there should be a notification popup
+                element(by.css('.alert-success')).element(by.css('pre')).getText().then((value) => {
+                    expect(value).toMatch(/A Source Type is deleted with identifier test-producer test-model v1/);
+                });
+            });
+        });
+    });
+
     afterAll(function () {
         accountMenu.click();
         logout.click();
