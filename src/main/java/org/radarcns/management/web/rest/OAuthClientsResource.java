a0688f461c018ad0c313d3b7bc17e56cf0d40a19
nivethika <nivemaham@gmail.com>
2017-12-15 12:22:41 +0100

    add variable for span class

--- src/main/java/org/radarcns/management/web/rest/OAuthClientsResource.java
+++ src/main/java/org/radarcns/management/web/rest/OAuthClientsResource.java
@@ -204,12 +204,12 @@ public class OAuthClientsResource {
         checkClientFields(clientDetailsDTO);
         // check if the client id exists
         try {
-            ClientDetails existing = clientDetailsService.loadClientByClientId(clientDetailsDTO
+            clientDetailsService.loadClientByClientId(clientDetailsDTO
                     .getClientId());
-            if(Objects.nonNull(existing)) {
-                throw new CustomConflictException("An OAuth client with that ID already exists",
+            // throw exception if a client already exist with a given id
+            throw new CustomConflictException("An OAuth client with that ID already exists",
                     Collections.singletonMap("client_id", clientDetailsDTO.getClientId()));
-            }
+
         } catch (NoSuchClientException ex) {
             // Client does not exist yet, we can go ahead and create it
         }
