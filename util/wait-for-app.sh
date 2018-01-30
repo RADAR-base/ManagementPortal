6a4fe9f75c43115f7866721288909250e51502c5
Denny Verbeeck <dennyverbeeck@gmail.com>
2018-01-30 17:24:53 +0100

    Limit wait time for application startup

--- util/wait-for-app.sh
+++ util/wait-for-app.sh
@@ -9,11 +9,19 @@ else
 fi
 
 # wait for app to start up
-echo "Waiting for application startup"
+echo "Waiting for application startup at $APP_URL"
+RETRY_COUNT=0
 until curl -s $APP_URL > /dev/null
 do
   echo -n "." # waiting
   sleep 2
+  ((RETRY_COUNT++))
+  if ((RETRY_COUNT > 120)) # wait max 120 times, so max 4 minutes
+  then
+    echo ""
+    echo "Application failed to start"
+    exit 1
+  fi
 done
 echo ""
 echo "Application is up"
