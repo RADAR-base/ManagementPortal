a940af93d0ce293cdd85f43b0bdfd7215c00acc2
Denny Verbeeck <dennyverbeeck@gmail.com>
2018-03-16 23:38:00 +0100

    Fix gatling tests, reduce their runtime

--- src/gatling/scala/ProjectGatlingTest.scala
+++ src/gatling/scala/ProjectGatlingTest.scala
@@ -8,10 +8,11 @@ import io.gatling.http.Predef._
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
+import scala.util.Random
 
 /**
- * Performance test for the Project entity.
- */
+  * Performance test for the Project entity.
+  */
 class ProjectGatlingTest extends Simulation {
 
     val context: LoggerContext = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
@@ -23,13 +24,13 @@ class ProjectGatlingTest extends Simulation {
     val baseURL = Option(System.getProperty("baseURL")) getOrElse """http://127.0.0.1:8080"""
 
     val httpConf = http
-        .baseURL(baseURL)
-        .inferHtmlResources()
-        .acceptHeader("*/*")
-        .acceptEncodingHeader("gzip, deflate")
-        .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
-        .connectionHeader("keep-alive")
-        .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0")
+      .baseURL(baseURL)
+      .inferHtmlResources()
+      .acceptHeader("*/*")
+      .acceptEncodingHeader("gzip, deflate")
+      .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+      .connectionHeader("keep-alive")
+      .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0")
 
     val headers_http = Map(
         "Accept" -> """application/json"""
@@ -48,53 +49,57 @@ class ProjectGatlingTest extends Simulation {
         "Authorization" -> "Bearer ${access_token}"
     )
 
+    var randomString = Iterator.continually(Map("randstring" -> ( Random.alphanumeric.take(12).mkString )))// length of the random string is 12 chars here
+
     val scn = scenario("Test the Project entity")
-        .exec(http("First unauthenticated request")
+      .exec(http("First unauthenticated request")
         .get("/api/account")
         .headers(headers_http)
         .check(status.is(401))).exitHereIfFailed
-        .pause(10)
-        .exec(http("Authentication")
+      .pause(5)
+      .exec(http("Authentication")
         .post("/oauth/token")
         .headers(headers_http_authentication)
         .formParam("username", "admin")
         .formParam("password", "admin")
         .formParam("grant_type", "password")
-        .formParam("scope", "read write")
         .formParam("client_secret", "my-secret-token-to-change-in-production")
         .formParam("client_id", "ManagementPortalapp")
         .formParam("submit", "Login")
         .check(jsonPath("$.access_token").saveAs("access_token"))).exitHereIfFailed
-        .pause(1)
-        .exec(http("Authenticated request")
+      .pause(1)
+      .exec(http("Authenticated request")
         .get("/api/account")
         .headers(headers_http_authenticated)
         .check(status.is(200)))
-        .pause(10)
-        .repeat(2) {
-            exec(http("Get all projects")
+      .pause(5)
+      .repeat(2) {
+          exec(http("Get all projects")
             .get("/api/projects")
             .headers(headers_http_authenticated)
             .check(status.is(200)))
-            .pause(10 seconds, 20 seconds)
+            .pause(5 seconds, 10 seconds)
+            .feed(randomString)
             .exec(http("Create new project")
-            .post("/api/projects")
-            .headers(headers_http_authenticated)
-            .body(StringBody("""{"id":null, "projectName":"SAMPLE_TEXT", "description":"SAMPLE_TEXT", "organization":"SAMPLE_TEXT", "location":"SAMPLE_TEXT", "startDate":"2020-01-01T00:00:00.000Z", "projectStatus":null, "endDate":"2020-01-01T00:00:00.000Z", "projectAdmin":null}""")).asJSON
-            .check(status.is(201))
-            .check(headerRegex("Location", "(.*)").saveAs("new_project_url"))).exitHereIfFailed
-            .pause(10)
+              .post("/api/projects")
+              .headers(headers_http_authenticated)
+              .body(StringBody("""{"id":null, "projectName":"PROJECT-${randstring}", "description":"SAMPLE_TEXT", "organization":"SAMPLE_TEXT", "location":"SAMPLE_TEXT", "startDate":"2020-01-01T00:00:00.000Z", "projectStatus":null, "endDate":"2020-01-01T00:00:00.000Z", "projectAdmin":null}""")).asJSON
+              .check(status.is(201))
+              .check(headerRegex("Location", "(.*)").saveAs("new_project_url"))).exitHereIfFailed
+            .pause(5)
             .repeat(5) {
                 exec(http("Get created project")
-                .get("${new_project_url}")
-                .headers(headers_http_authenticated))
-                .pause(10)
+                  .get("${new_project_url}")
+                  .headers(headers_http_authenticated)
+                  .check(status.is(200)))
+                  .pause(5)
             }
             .exec(http("Delete created project")
-            .delete("${new_project_url}")
-            .headers(headers_http_authenticated))
-            .pause(10)
-        }
+              .delete("${new_project_url}")
+              .headers(headers_http_authenticated)
+              .check(status.is(200)))
+            .pause(5)
+      }
 
     val users = scenario("Users").exec(scn)
 
