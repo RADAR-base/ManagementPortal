16f798afa04bdf06eb9e9a9a670f673a03f7882b
Denny Verbeeck <dennyverbeeck@gmail.com>
2018-03-23 15:41:19 +0100

    Add SYS_ADMIN security level to revision api

--- src/main/java/org/radarcns/management/web/rest/RevisionResource.java
+++ src/main/java/org/radarcns/management/web/rest/RevisionResource.java
@@ -2,6 +2,7 @@ package org.radarcns.management.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
 import io.swagger.annotations.ApiParam;
+import org.radarcns.auth.authorization.AuthoritiesConstants;
 import org.radarcns.management.service.RevisionService;
 import org.radarcns.management.service.dto.RevisionInfoDTO;
 import org.radarcns.management.web.rest.util.PaginationUtil;
@@ -10,9 +11,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,11 +38,12 @@ public class RevisionResource {
      */
     @GetMapping("/revisions")
     @Timed
+    @Secured({AuthoritiesConstants.SYS_ADMIN})
     public ResponseEntity<List<RevisionInfoDTO>> getRevisions(@ApiParam Pageable pageable) {
         log.debug("REST request to get page of revisions");
         Page<RevisionInfoDTO> page = revisionService.getRevisions(pageable);
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/revisions");
-        return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
+        return new ResponseEntity<>(page.getContent(), PaginationUtil
+                .generatePaginationHttpHeaders(page, "/api/revisions"), HttpStatus.OK);
     }
 
     /**
@@ -52,6 +54,7 @@ public class RevisionResource {
      */
     @GetMapping("/revisions/{id}")
     @Timed
+    @Secured({AuthoritiesConstants.SYS_ADMIN})
     public ResponseEntity<RevisionInfoDTO> getRevision(@PathVariable("id") Long id) {
         log.debug("REST request to get single revision: {}", id.toString());
         return ResponseEntity.ok(revisionService.getRevision(id));
