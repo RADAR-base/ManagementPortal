9d5600004c18d62e66ca7a2c71cb2b28293f973a
nivethika <nivemaham@gmail.com>
2018-07-04 09:03:50 +0200

    improvements

--- src/main/java/org/radarcns/management/hibernate/CaseSensitivePhysicalNamingStrategy.java
+++ src/main/java/org/radarcns/management/hibernate/CaseSensitivePhysicalNamingStrategy.java
@@ -3,7 +3,7 @@ package org.radarcns.management.hibernate;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 
-public class RadarPhysicalNamingStrategy extends SpringPhysicalNamingStrategy {
+public class CaseSensitivePhysicalNamingStrategy extends SpringPhysicalNamingStrategy {
 
     @Override
     protected boolean isCaseInsensitive(JdbcEnvironment jdbcEnvironment) {
