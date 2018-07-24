ca4a17f6b0381829f6b2efff0b5047a260e6d6f3
nivethika <nivemaham@gmail.com>
2018-07-24 17:38:43 +0200

    change type of token and add aud table

--- src/main/java/org/radarcns/management/domain/MetaToken.java
+++ src/main/java/org/radarcns/management/domain/MetaToken.java
@@ -1,6 +1,6 @@
 package org.radarcns.management.domain;
 
-import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
@@ -10,12 +10,11 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.envers.Audited;
-import org.radarcns.auth.config.Constants;
 import org.radarcns.management.domain.support.AbstractEntityListener;
 
 @Entity
@@ -25,21 +24,28 @@ import org.radarcns.management.domain.support.AbstractEntityListener;
 @EntityListeners({AbstractEntityListener.class})
 public class MetaToken extends AbstractEntity {
 
+    private static final int SHORT_ID_LENGTH = 12;
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     @SequenceGenerator(name = "sequenceGenerator", initialValue = 1000)
     private Long id;
 
     @NotNull
-    @Pattern(regexp = Constants.TOKEN_NAME_REGEX)
     @Column(name = "token_name", nullable = false, unique = true)
     private String tokenName;
 
-    @Column(name = "token", length = 65535,columnDefinition="Text")
+    @Column(name = "token", length = 2000)
     private String token;
 
     @Column(name = "fetched", nullable = false)
     private Boolean fetched;
+
+    public MetaToken () {
+        if (this.tokenName == null) {
+            this.tokenName = RandomStringUtils.randomAlphanumeric(SHORT_ID_LENGTH);
+        }
+    }
 
     public Long getId() {
         return id;
@@ -71,5 +77,33 @@ public class MetaToken extends AbstractEntity {
 
     public void setFetched(boolean fetched) {
         this.fetched = fetched;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MetaToken token1 = (MetaToken) o;
+        return Objects.equals(id, token1.id) && Objects.equals(tokenName, token1.tokenName)
+            && Objects.equals(fetched, token1.fetched);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, tokenName, token, fetched);
+    }
+
+    @Override
+    public String toString() {
+        return "MetaToken{"
+            + "id=" + id + ","
+            + " tokenName='" + tokenName + '\''
+            + ", token='" + token + '\''
+            + ", fetched=" + fetched + '}';
     }
 }
