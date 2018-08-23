684666dd88f277912acf99a44be2ad55fd74b86f
nivethika <nivemaham@gmail.com>
2018-08-23 10:49:08 +0200

    remove sleep from metatoken fetch and add documentation

--- src/main/java/org/radarcns/management/web/rest/MetaTokenResource.java
+++ src/main/java/org/radarcns/management/web/rest/MetaTokenResource.java
@@ -3,7 +3,6 @@ package org.radarcns.management.web.rest;
 
 import java.net.MalformedURLException;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 import com.codahale.metrics.annotation.Timed;
 import org.radarcns.auth.config.Constants;
@@ -13,7 +12,6 @@ import org.radarcns.management.service.dto.TokenDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,19 +42,6 @@ public class MetaTokenResource {
     public ResponseEntity<TokenDTO> getTokenByTokenName(@PathVariable("tokenName") String tokenName)
             throws MalformedURLException {
         log.info("Requesting token with tokenName {}", tokenName);
-        try {
-            // sleep for 3 seconds
-            // makes brute-force attacks too expensive to be worthwhile.
-            // For every guess, an attacker needs to wait X seconds before a response is returned.
-            // This needs to be done after both valid and invalid requests, to ensure that an
-            // attacker does not define an internal time threshold that would only be reached
-            // for invalid URLs.
-            TimeUnit.SECONDS.sleep(3);
-            TokenDTO retrievedToken = metaTokenService.fetchToken(tokenName);
-            return ResponseEntity.ok().body(retrievedToken);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        return ResponseEntity.ok().body(metaTokenService.fetchToken(tokenName));
     }
 }
