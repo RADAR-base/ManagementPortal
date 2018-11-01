be7cdeabd6d8751e11fbdd9cf141116b82dbe2b5
Joris Borgdorff <joris@thehyve.nl>
2018-11-01 10:40:38 +0100

    Add back missing convertSeconds function

--- src/main/webapp/app/entities/oauth-client/oauth-client.component.ts
+++ src/main/webapp/app/entities/oauth-client/oauth-client.component.ts
@@ -65,4 +65,24 @@ export class OAuthClientComponent implements OnInit, OnDestroy {
     private onError(error) {
         this.alertService.error(error.message, null, null);
     }
+
+    private convertSeconds(seconds: number) {
+        let time_string: string = '';
+        let plural: string = '';
+        for(var key in this.times){
+            if(Math.floor(seconds / this.times[key]) > 0){
+                if(Math.floor(seconds / this.times[key]) >1 ){
+                    plural = 's';
+                }
+                else{
+                    plural = '';
+                }
+
+                time_string += Math.floor(seconds / this.times[key]).toString() + ' ' + key.toString() + plural + ' ';
+                seconds = seconds - this.times[key] * Math.floor(seconds / this.times[key]);
+
+            }
+        }
+        return time_string;
+    }
 }
