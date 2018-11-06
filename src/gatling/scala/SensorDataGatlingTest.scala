04b4ac61079e53916282a4cfcf9f5f97b56c96a3
Joris Borgdorff <joris@thehyve.nl>
2018-11-06 10:43:15 +0100

    Merge scala setup

--- src/gatling/scala/SensorDataGatlingTest.scala
+++ src/gatling/scala/SensorDataGatlingTest.scala
@@ -1,57 +1,16 @@
 
-import java.nio.charset.StandardCharsets
-import java.util.Base64
-
-import ch.qos.logback.classic.LoggerContext
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
-import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
-import scala.util.Random
 
 /**
   * Performance test for the SourceData entity.
   */
-class SensorDataGatlingTest extends Simulation {
+class SensorDataGatlingTest extends ManagementPortalSimulation {
 
-    val context: LoggerContext = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
-    // Log all HTTP requests
-    //context.getLogger("io.gatling.http").setLevel(Level.valueOf("TRACE"))
-    // Log failed HTTP requests
-    //context.getLogger("io.gatling.http").setLevel(Level.valueOf("DEBUG"))
-
-    val baseURL = Option(System.getProperty("baseURL")) getOrElse """http://127.0.0.1:8080"""
-
-    val httpConf = http
-      .baseURL(baseURL)
-      .inferHtmlResources()
-      .acceptHeader("*/*")
-      .acceptEncodingHeader("gzip, deflate")
-      .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
-      .connectionHeader("keep-alive")
-      .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0")
-
-    val headers_http = Map(
-        "Accept" -> """application/json"""
-    )
-
-    val authorization_header = "Basic " + Base64.getEncoder.encodeToString("ManagementPortalapp:my-secret-token-to-change-in-production".getBytes(StandardCharsets.UTF_8))
-
-    val headers_http_authentication = Map(
-        "Content-Type" -> """application/x-www-form-urlencoded""",
-        "Accept" -> """application/json""",
-        "Authorization"-> authorization_header
-    )
-
-    val headers_http_authenticated = Map(
-        "Accept" -> """application/json""",
-        "Authorization" -> "Bearer ${access_token}"
-    )
-
-    val randomString = Iterator.continually(Map("randstring" -> ( Random.alphanumeric.take(12).mkString )))// length of the random string is 12 chars here
-
-    val scn = scenario("Test the SourceData entity")
+    override val scn: ScenarioBuilder = scenario("Test the SourceData entity")
       .exec(http("First unauthenticated request")
         .get("/api/account")
         .headers(headers_http)
@@ -101,9 +60,5 @@ class SensorDataGatlingTest extends Simulation {
             .pause(5)
       }
 
-    val users = scenario("Users").exec(scn)
-
-    setUp(
-        users.inject(rampUsers(100) over (1 minutes))
-    ).protocols(httpConf)
+    run()
 }
