08bcbe754c0cd1c6533f269ab5625aad6eb5131b
nivethika <nivemaham@gmail.com>
2018-11-28 15:02:15 +0100

    comments based on PR review

--- src/main/java/org/radarcns/management/service/MailService.java
+++ src/main/java/org/radarcns/management/service/MailService.java
@@ -104,7 +104,7 @@ public class MailService {
         context.setVariable(USER, user);
         context.setVariable(BASE_URL,
                 managementPortalProperties.getCommon().getManagementPortalBaseUrl());
-        context.setVariable(EXPIRY, Duration.ofSeconds(duration).toHours() + " Hours");
+        context.setVariable(EXPIRY, Duration.ofSeconds(duration).toHours());
         String content = templateEngine.process("creationEmail", context);
         String subject = messageSource.getMessage("email.activation.title", null, locale);
         sendEmail(user.getEmail(), subject, content, false, true);
