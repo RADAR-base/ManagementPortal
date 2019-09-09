b9e530a9c2ce3b857808953744ce995615030df9
nivethika <nivemaham@gmail.com>
2019-09-09 17:01:50 +0200

    try with webdriver-manager update

--- src/test/bash/run-e2e.sh
+++ src/test/bash/run-e2e.sh
@@ -5,6 +5,7 @@ if [ -z $TRAVIS_TAG ]
 then
   echo "Running e2e tests"
   ls -al node_modules/webdriver-manager/selenium/
+  yarn webdriver-manager update
   yarn e2e
 else
   echo "Skipping e2e tests on tag builds"
