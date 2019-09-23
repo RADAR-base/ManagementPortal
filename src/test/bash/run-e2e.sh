f44f519f7456a6a80d1514de10bca949c7765159
nivethika <nivemaham@gmail.com>
2019-09-23 17:13:51 +0200

    try running webdriver update with specific version

--- src/test/bash/run-e2e.sh
+++ src/test/bash/run-e2e.sh
@@ -4,7 +4,7 @@
 if [ -z $TRAVIS_TAG ]
 then
   echo "Running e2e tests"
-  yarn webdriver-manager update
+  yarn webdriver-manager update --versions.chrome 77.0.3865.10
   yarn e2e
 else
   echo "Skipping e2e tests on tag builds"
