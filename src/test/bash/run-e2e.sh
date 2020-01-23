7c003569ed63eac5b45cfd625128e3ca210340ee
mpgxvii <mpgxvii@gmail.com>
2020-01-23 16:03:46 +0000

    Revert e2e script

--- src/test/bash/run-e2e.sh
+++ src/test/bash/run-e2e.sh
@@ -5,7 +5,7 @@ if [ -z "$TRAVIS_TAG" ]
 then
   echo "Running e2e tests"
   yarn webdriver-manager update --versions.chrome "$(google-chrome --version | grep -iE "[0-9.]{10,20}")"
-  xvfb-run -a yarn e2e
+  yarn e2e
 else
   echo "Skipping e2e tests on tag builds"
 fi
