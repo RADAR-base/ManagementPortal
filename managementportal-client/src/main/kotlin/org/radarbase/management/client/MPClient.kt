f14719900bc7bea729df2b7c7f7589eaade03d76
Joris Borgdorff <joris@thehyve.nl>
2021-01-11 12:10:03 +0100

    Add documentation

--- managementportal-client/src/main/kotlin/org/radarbase/management/client/MPClient.kt
+++ managementportal-client/src/main/kotlin/org/radarbase/management/client/MPClient.kt
@@ -23,14 +23,20 @@ import okhttp3.Request
 import okhttp3.Response
 import org.radarcns.oauth.OAuth2AccessTokenDetails
 import org.radarcns.oauth.OAuth2Client
-import org.slf4j.LoggerFactory
 import java.io.IOException
 import java.net.MalformedURLException
 import java.util.concurrent.TimeUnit
 
+/**
+ * Client for the ManagementPortal REST API.
+ */
+@Suppress("unused")
 class MPClient private constructor(
+    /** Server configuration of the ManagementPortal API. */
     serverConfig: MPServerConfig,
+    /** ObjectMapper to use for all requests. */
     objectMapper: ObjectMapper? = null,
+    /** HTTP client to make requests with. */
     httpClient: OkHttpClient? = null,
 ) {
     private val clientId: String = serverConfig.clientId
@@ -48,6 +54,7 @@ class MPClient private constructor(
         .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
+    /** HTTP client to make requests with. */
     var httpClient: OkHttpClient = httpClient ?: OkHttpClient().newBuilder()
         .connectTimeout(10, TimeUnit.SECONDS)
         .writeTimeout(10, TimeUnit.SECONDS)
@@ -64,60 +71,104 @@ class MPClient private constructor(
         .endpoint(baseUrl.toUrl(), "oauth/token")
         .build()
 
+    /**
+     * Valid access token for the ManagementPortal REST API.
+     * @throws org.radarcns.exception.TokenException if a new access token could not be fetched
+     */
     val validToken: OAuth2AccessTokenDetails
         get() = oauth2Client.validToken
 
-    /** Read list of projects from ManagementPortal. */
-    fun readProjects(
+    /** Request list of projects from ManagementPortal. */
+    fun requestProjects(
         page: Int = 0,
         size: Int = Int.MAX_VALUE,
-    ): List<MPProject> {
-        logger.debug("Requesting for projects")
-        return request(projectListReader,
-            {
-                addPathSegments("api/projects")
-                addQueryParameter("page", page.toString())
-                addQueryParameter("size", size.toString())
-            })
-    }
+    ): List<MPProject> = request(
+        projectListReader,
+        {
+            addPathSegments("api/projects")
+            addQueryParameter("page", page.toString())
+            addQueryParameter("size", size.toString())
+        })
 
-    /** Read list of participants from ManagementPortal project. The [projectId] is the name that
-     * the project is identified by. */
-    fun readSubjects(
+    /**
+     * Request list of subjects from ManagementPortal project. The [projectId] is the name that
+     * the project is identified by.
+     */
+    fun requestSubjects(
         projectId: String,
         page: Int = 0,
         size: Int = Int.MAX_VALUE,
-    ): List<MPSubject> {
-        return request<List<MPSubject>>(subjectListReader,
-            {
-                addPathSegments("api/projects/$projectId/subjects")
-                addQueryParameter("page", page.toString())
-                addQueryParameter("size", size.toString())
-            })
-            .map { it.copy(projectId = projectId) }
-    }
+    ): List<MPSubject> = request<List<MPSubject>>(
+        subjectListReader,
+        {
+            addPathSegments("api/projects/$projectId/subjects")
+            addQueryParameter("page", page.toString())
+            addQueryParameter("size", size.toString())
+        })
+        .map { it.copy(projectId = projectId) }
 
-    @Suppress("unused")
-    fun readClients(): List<MPOAuthClient> {
-        return request(clientListReader, { addPathSegments("api/oauth-clients") })
-    }
+    /**
+     * Request list of OAuth 2.0 clients from ManagementPortal.
+     */
+    fun requestClients(
+        page: Int = 0,
+        size: Int = Int.MAX_VALUE,
+    ): List<MPOAuthClient> = request(
+        clientListReader,
+        {
+            addPathSegments("api/oauth-clients")
+            addQueryParameter("page", page.toString())
+            addQueryParameter("size", size.toString())
+        })
 
+    /**
+     * Make a request and parse the result as JSON. The response body is parsed by [reader]. The
+     * url to query is constructed using [urlBuilder]. In [urlBuilder], use
+     * [HttpUrl.Builder.addPathSegments] over [HttpUrl.Builder.encodedPath] to preserve the correct
+     * base URL. The request can optionally be modified with [requestBuilder], for example to POST
+     * content or add headers.
+     * @throws IOException if the request fails, has a unsuccessful status code or if the response
+     *      cannot be read.
+     */
     inline fun <T> request(
         reader: ObjectReader,
         urlBuilder: HttpUrl.Builder.() -> Unit,
         requestBuilder: (Request.Builder.() -> Unit) = { }
     ): T = request(urlBuilder, requestBuilder) { request, response ->
+        if (!response.isSuccessful) {
+            throw IOException("Request to ${request.url} failed (code ${response.code})")
+        }
         val body = response.body ?: throw IOException("No response body to ${request.url}")
         reader.readValue(body.byteStream())
     }
 
+    /**
+     * Make a request without any response processing. The
+     * url to query is constructed using [urlBuilder]. In [urlBuilder], use
+     * [HttpUrl.Builder.addPathSegments] over [HttpUrl.Builder.encodedPath] to preserve the correct
+     * base URL. The request can optionally be modified with [requestBuilder], for example to POST
+     * content or add headers.
+     * @throws IOException if the request fails or has a unsuccessful status code.
+     */
     inline fun request(
         urlBuilder: HttpUrl.Builder.() -> Unit,
         requestBuilder: (Request.Builder.() -> Unit) = { },
     ) {
-        request(urlBuilder, requestBuilder, { _, _ -> })
+        request(urlBuilder, requestBuilder, { request, response ->
+            if (!response.isSuccessful) {
+                throw IOException("Request to ${request.url} failed (code ${response.code})")
+            }
+        })
     }
 
+    /**
+     * Make a request without custom response processing. The
+     * url to query is constructed using [urlBuilder]. In [urlBuilder], use
+     * [HttpUrl.Builder.addPathSegments] over [HttpUrl.Builder.encodedPath] to preserve the correct
+     * base URL. The request can optionally be modified with [requestBuilder], for example to POST
+     * content or add headers. The response can be handled with [responseHandler], e.g., by
+     * evaluating the status code or the body with a custom body reader.
+     */
     inline fun <T> request(
         urlBuilder: HttpUrl.Builder.() -> Unit,
         requestBuilder: Request.Builder.() -> Unit = { },
@@ -132,13 +183,8 @@ class MPClient private constructor(
         }.build()
 
         return httpClient.newCall(request).execute().use {
-            if (!it.isSuccessful) throw IOException("Request to ${request.url} failed (code ${it.code})")
             responseHandler(request, it)
         }
-    }
-
-    companion object {
-        private val logger = LoggerFactory.getLogger(MPClient::class.java)
     }
 
     data class MPServerConfig(
