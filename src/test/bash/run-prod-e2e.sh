ddaee08c9d2f2ee240094fb72b94b8097c980cdc
Joris Borgdorff <joris@thehyve.nl>
2021-04-12 17:28:16 +0200

    Test fixes

--- src/test/bash/run-prod-e2e.sh
+++ src/test/bash/run-prod-e2e.sh
@@ -12,20 +12,21 @@ set -e
 
 # only run on the release branch and master branch if it's not a tag build
 echo "Running production e2e tests"
+cp src/test/resources/config/keystore.p12 src/main/docker/etc/config
+cp src/test/resources/config/keystore.p12 src/main/resources/config
 yarn run webpack:prod
 sed -i "s|new plugin.BaseHrefWebpackPlugin({ baseHref: '/' })|new plugin.BaseHrefWebpackPlugin({ baseHref: '/managementportal/' })|" webpack/webpack.dev.js
 sed -i "s|baseUrl: 'http://localhost:8080/',|baseUrl: 'http://localhost:8080/managementportal/',|" src/test/javascript/protractor.conf.js
 sed -i "s|contexts: prod|contexts: dev|" src/main/resources/config/application-prod.yml # set liquibase context to dev so it loads demo data
-cp src/test/resources/config/keystore.p12 src/main/docker/etc/config
 ./gradlew bootRepackage -Pprod buildDocker -x test
 
 docker-compose -f src/main/docker/app.yml up -d # spin up production mode application
 set +e
 
 # wait for app to be up
-yarn run wait-for-managementportal
+yarn run wait-for-managementportal-prod
 # run e2e tests against production mode
-if ./gradlew generateOpenApiSpec && yarn e2e; then
+if ./gradlew generateOpenApiSpec && yarn e2e-prod; then
   EXIT_STATUS=0
 else
   EXIT_STATUS=1
