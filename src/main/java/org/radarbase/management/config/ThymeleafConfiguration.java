d468f7b476e443571a59da7b7dae45134c71836c
mpgxvii <mpgxvii@gmail.com>
2021-05-11 02:23:23 +0800

    Move thymeleaf templates to single directory to fix TemplateInputException

--- src/main/java/org/radarbase/management/config/ThymeleafConfiguration.java
+++ src/main/java/org/radarbase/management/config/ThymeleafConfiguration.java
@@ -12,7 +12,7 @@ public class ThymeleafConfiguration {
     @Description("Thymeleaf template resolver serving HTML 5 emails")
     public ClassLoaderTemplateResolver emailTemplateResolver() {
         ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
-        emailTemplateResolver.setPrefix("mails/");
+        emailTemplateResolver.setPrefix("templates/");
         emailTemplateResolver.setSuffix(".html");
         emailTemplateResolver.setTemplateMode("HTML");
         emailTemplateResolver.setCharacterEncoding(CharEncoding.UTF_8);
