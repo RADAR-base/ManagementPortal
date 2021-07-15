cd9b85cbe5eb58277378c8c8079236f5b04a707d
aykl <alexejkhl@gmail.com>
2021-07-15 19:28:36 +0200

    JhiTranslate -> NgxTranslate

--- src/main/webapp/app/shared/user/common-user-management.component.ts
+++ src/main/webapp/app/shared/user/common-user-management.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
-import { JhiLanguageService } from 'ng-jhipster';
 
 import { ITEMS_PER_PAGE, Project, User, UserService } from '..';
 import { EventManager } from '../util/event-manager.service';
@@ -24,12 +23,10 @@ export class CommonUserMgmtComponent implements OnInit, OnChanges {
     @Input() authority: String;
 
     constructor(
-            private jhiLanguageService: JhiLanguageService,
             private userService: UserService,
             private eventManager: EventManager,
     ) {
         this.itemsPerPage = ITEMS_PER_PAGE;
-        this.jhiLanguageService.addLocation('user-management');
     }
 
     ngOnInit() {
