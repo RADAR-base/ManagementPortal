7f0b15b76c1b99c37fe23c0624ee9e7ad31c3b14
aykl <alexejkhl@gmail.com>
2021-08-07 16:20:31 +0200

    JhiTranslate -> NgxTranslate for Source Detail

--- src/main/webapp/app/shared/source/source-detail.component.ts
+++ src/main/webapp/app/shared/source/source-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { JhiLanguageService } from 'ng-jhipster';
 import { Subscription } from 'rxjs/Rx';
 
 import { EventManager } from '../util/event-manager.service';
@@ -19,11 +18,9 @@ export class SourceDetailComponent implements OnInit, OnDestroy {
 
     constructor(
             private eventManager: EventManager,
-            private jhiLanguageService: JhiLanguageService,
             private sourceService: SourceService,
             private route: ActivatedRoute,
     ) {
-        this.jhiLanguageService.setLocations(['source']);
     }
 
     ngOnInit() {
