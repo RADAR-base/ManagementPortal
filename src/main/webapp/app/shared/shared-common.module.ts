78d2b80e67de10f11c6904a89b577dab521b95c1
aykl <alexejkhl@gmail.com>
2021-08-18 09:09:02 +0200

    NgJhipster removed

--- src/main/webapp/app/shared/shared-common.module.ts
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -1,17 +1,11 @@
-import { NgModule, Sanitizer } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import {
     FindLanguageFromKeyPipe,
     JhiAlertComponent,
     JhiAlertErrorComponent,
-    JhiLanguageHelper,
     ManagementPortalSharedLibsModule,
 } from './';
-import { AlertService } from './util/alert.service';
-
-export function alertServiceProvider(sanitizer: Sanitizer) {
-    return new AlertService(sanitizer);
-}
 
 @NgModule({
     imports: [
@@ -23,12 +17,6 @@ export function alertServiceProvider(sanitizer: Sanitizer) {
         JhiAlertErrorComponent,
     ],
     providers: [
-        JhiLanguageHelper,
-        {
-            provide: AlertService,
-            useFactory: alertServiceProvider,
-            deps: [Sanitizer],
-        },
         Title,
     ],
     exports: [
--- src/main/webapp/app/shared/shared-libs.module.ts
+++ src/main/webapp/app/shared/shared-libs.module.ts
@@ -2,23 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgJhipsterModule } from 'ng-jhipster';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     imports: [
         NgbModule.forRoot(),
-        NgJhipsterModule.forRoot({
-            i18nEnabled: true,
-            defaultI18nLang: 'en',
-        }),
     ],
     exports: [
         FormsModule,
         HttpClientModule,
         CommonModule,
         NgbModule,
-        NgJhipsterModule,
     ],
 })
 export class ManagementPortalSharedLibsModule {
