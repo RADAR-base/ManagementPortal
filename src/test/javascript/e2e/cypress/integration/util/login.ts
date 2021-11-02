89aceb9034c96e178a7e1ecadbea4a0e263f655d
Joris Borgdorff <joris@thehyve.nl>
2021-11-02 12:12:59 +0100

    Add cy.session again

--- src/test/javascript/e2e/cypress/integration/util/login.ts
+++ src/test/javascript/e2e/cypress/integration/util/login.ts
@@ -1,13 +1,17 @@
 import * as navBarPage from "./nav-bar";
 
 export function login(username = "admin", password = "admin") {
-    cy.visit('./');
-    navBarPage.clickOnAccountMenu();
-    navBarPage.clickOnSignIn();
-    cy.get('.modal-content h1').first().should('have.text', 'Sign in');
-    cy.get('#username').type(username);
-    cy.get('#password').type(password);
-    cy.get('button[type=submit]').click();
-    cy.get('.alert-success span').should('exist');
-    cy.getCookie('SESSION').should('exist')
+    cy.session([username, password], () => {
+        cy.visit('/');
+        navBarPage.clickOnAccountMenu();
+        navBarPage.clickOnSignIn();
+        cy.get('.modal-content h1').first().should('have.text', 'Sign in');
+        cy.get('#username').type(username);
+        cy.get('#password').type(password);
+        cy.get('button[type=submit]').click();
+        cy.get('.alert-success span').should('exist');
+    }, { validate() {
+        cy.getCookie('SESSION').should('exist')}
+    })
+    cy.visit('/');
 }
