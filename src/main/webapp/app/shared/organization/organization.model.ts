64e60e7bcd77998ff818d55557759136c36e9cb3
aykl <alexejkhl@gmail.com>
2021-11-10 15:04:47 +0100

    OrganizationService added

---
+++ src/main/webapp/app/shared/organization/organization.model.ts
@@ -0,0 +1,10 @@
+
+export class Organization {
+    constructor(
+        public id?: number,
+        public name?: string,
+        public description?: string,
+        public location?: string,
+    ) {
+    }
+}
