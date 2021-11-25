82fb2f9454d1255b76fa16afbc7787c1fee3da3c
Joris Borgdorff <joris@thehyve.nl>
2021-11-25 16:21:33 +0100

    More lenient handling of catalog server responses

--- src/main/java/org/radarbase/management/service/catalog/SampleRateConfig.java
+++ src/main/java/org/radarbase/management/service/catalog/SampleRateConfig.java
@@ -1,7 +1,9 @@
 package org.radarbase.management.service.catalog;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SampleRateConfig {
 
     @JsonProperty
