769e18782d84de50b6f189c26870cbf29b777745
Joris Borgdorff <joris@thehyve.nl>
2021-12-02 16:33:25 +0100

    Properly map organizations to projects and vice-versa

--- src/main/java/org/radarbase/management/service/mapper/decorator/ProjectMapperDecorator.java
+++ src/main/java/org/radarbase/management/service/mapper/decorator/ProjectMapperDecorator.java
@@ -79,13 +79,13 @@ public abstract class ProjectMapperDecorator implements ProjectMapper {
             project.getAttributes().put(HUMAN_READABLE_PROJECT_NAME, projectName);
         }
 
-        var orgName = projectDto.getOrganizationName();
-        if (orgName != null) {
-            var org = organizationRepository.findOneByName(orgName).orElseThrow(
+        var orgDto = projectDto.getOrganization();
+        if (orgDto != null && orgDto.getName() != null) {
+            var org = organizationRepository.findOneByName(orgDto.getName()).orElseThrow(
                     () -> new NotFoundException("Organization not found with name",
                             ORGANIZATION,
                             ErrorConstants.ERR_ORGANIZATION_NAME_NOT_FOUND,
-                            Collections.singletonMap("name", orgName)));
+                            Collections.singletonMap("name", orgDto.getName())));
             project.setOrganization(org);
         }
 
