94913885ab6a929370a0e8c62015f26cac5fbd40
Peyman Mohtashami <p.mohtashami@gmail.com>
2022-08-22 12:31:26 +0200

    Add ORG_ADMIN role to routes

--- src/main/webapp/app/account/password/password.route.ts
+++ src/main/webapp/app/account/password/password.route.ts
@@ -1,14 +1,14 @@
 import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from '../../shared';
-import { PROJECT_ADMIN, SYSTEM_ADMIN } from '../../shared/constants/common.constants';
+import {ORGANIZATION_ADMIN, PROJECT_ADMIN, SYSTEM_ADMIN} from '../../shared/constants/common.constants';
 import { PasswordComponent } from './password.component';
 
 export const passwordRoute: Route = {
     path: 'password',
     component: PasswordComponent,
     data: {
-        authorities: [SYSTEM_ADMIN, PROJECT_ADMIN, 'ROLE_USER'],
+        authorities: [SYSTEM_ADMIN, ORGANIZATION_ADMIN, PROJECT_ADMIN, 'ROLE_USER'],
         pageTitle: 'global.menu.account.password',
     },
     canActivate: [UserRouteAccessService],
--- src/main/webapp/app/account/settings/settings.route.ts
+++ src/main/webapp/app/account/settings/settings.route.ts
@@ -1,14 +1,14 @@
 import { Route } from '@angular/router';
 
 import { UserRouteAccessService } from '../../shared';
-import { PROJECT_ADMIN, SYSTEM_ADMIN } from '../../shared/constants/common.constants';
+import {ORGANIZATION_ADMIN, PROJECT_ADMIN, SYSTEM_ADMIN} from '../../shared/constants/common.constants';
 import { SettingsComponent } from './settings.component';
 
 export const settingsRoute: Route = {
     path: 'settings',
     component: SettingsComponent,
     data: {
-        authorities: [SYSTEM_ADMIN, PROJECT_ADMIN],
+        authorities: [SYSTEM_ADMIN, ORGANIZATION_ADMIN, PROJECT_ADMIN],
         pageTitle: 'global.menu.account.settings',
     },
     canActivate: [UserRouteAccessService],
