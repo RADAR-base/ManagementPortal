5a11238d1e1c3b1d93a7d0ff91589c668363db02
Bastiaan <bastiaan@thehyve.nl>
2023-09-21 13:57:05 +0200

    moved http get request in sitesettings.service.ts to prevent redundant requests. Added input checking.

--- src/main/webapp/app/shared/subject/sitesettings.service.ts
+++ src/main/webapp/app/shared/subject/sitesettings.service.ts
@@ -1,18 +1,40 @@
 import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
+import {BehaviorSubject, Observable, of} from "rxjs";
 import {SiteSettings} from "./subject.model";
 import {HttpClient} from "@angular/common/http";
+import {map, share, switchMap, tap} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class SiteSettingsService {
 
     private siteSettingsResourceUrl = 'api/sitesettings';
-
+    private _siteSettings$ = new BehaviorSubject<SiteSettings>(null);
+    siteSettings$ = new Observable<SiteSettings>(null);
 
     constructor(private http: HttpClient) {
+        this.siteSettings$ = this._siteSettings$.pipe(
+            switchMap(s => s ? of(s) : this.fetchSiteSettings()),
+            share(),
+        );
     }
 
-    getSiteSettings(): Observable<SiteSettings> {
-        return this.http.get<SiteSettings>(this.siteSettingsResourceUrl);
+    private fetchSiteSettings() {
+        return this.http.get<SiteSettings>(this.siteSettingsResourceUrl).pipe(
+            map(s => this.checkAndClean(s)),
+            tap(s => this._siteSettings$.next(s)),
+        );
+    }
+
+    private checkAndClean(s: SiteSettings) : SiteSettings {
+        if (s != null &&
+            typeof s == 'object' &&
+            Array.isArray(s.hiddenSubjectFields)
+        ){
+            return s;
+        }
+        else
+            return new class implements SiteSettings {
+                hiddenSubjectFields: string[];
+            }
     }
 }
