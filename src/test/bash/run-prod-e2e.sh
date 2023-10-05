bde4c4ec6dd3d8e7c41a9966971161a45affb29c
Bastiaan <bastiaan@thehyve.nl>
2023-10-05 10:20:32 +0200

    we make it to the e2e test

--- src/test/bash/run-prod-e2e.sh
+++ src/test/bash/run-prod-e2e.sh
@@ -27,7 +27,7 @@ set +e
 # wait for app to be up
 yarn run wait-for-managementportal-prod
 # run e2e tests against production mode
-if ./gradlew generateOpenApiSpecProd && yarn run e2e-prod; then
+if ./gradlew generateOpenApiSpec && yarn run e2e-prod; then
   EXIT_STATUS=0
 else
   EXIT_STATUS=1
