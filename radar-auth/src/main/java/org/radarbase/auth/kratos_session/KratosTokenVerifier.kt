4e36067b553ac31b9e3325fbb4f5362fb4f1b9b3
Bastiaan <bastiaan@thehyve.nl>
2023-10-24 15:52:35 +0200

    all verify functions should be marked 'suspend'

--- radar-auth/src/main/java/org/radarbase/auth/kratos_session/KratosTokenVerifier.kt
+++ radar-auth/src/main/java/org/radarbase/auth/kratos_session/KratosTokenVerifier.kt
@@ -1,5 +1,4 @@
-package org.radarbase.auth.kratos_session
-
+import kotlinx.serialization.encoding.Decoder
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -8,8 +7,14 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.util.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.json.Json
 import org.radarbase.auth.authentication.TokenVerifier
 import org.radarbase.auth.authorization.AuthorityReference
@@ -20,6 +25,8 @@ import org.radarbase.auth.token.RadarToken
 import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.time.Instant
+import java.util.*
+import kotlin.collections.ArrayList
 
 class KratosTokenVerifier : TokenVerifier {
     private val httpClient = HttpClient(CIO).config {
@@ -38,67 +45,72 @@ class KratosTokenVerifier : TokenVerifier {
 
     val kratosBaseUrl = ("http://localhost:" + "4434")
 
-    override fun verify(token: String): RadarToken = try {
+    override suspend fun verify(token: String): RadarToken = try {
         val cookie = "ory_kratos_session=" + token
 
-        var kratosSession: KratosDTO? = null
+        val kratosSession: KratosDTO
 
-        runBlocking {
-            val response = httpClient.get{
+        withContext(Dispatchers.IO) {
+            val response = httpClient.get {
                 header("Cookie", cookie)
                 url("$kratosBaseUrl/sessions/whoami")
                 accept(ContentType.Application.Json)
             }
 
             if (response.status.isSuccess()) {
+                logger.debug(response.body())
                 kratosSession = response.body<KratosDTO>()
-            }
-            else {
+            } else {
                 throw TokenValidationException("couldn't get kratos session")
             }
         }
 
-
-        if (kratosSession != null) {
-            DataRadarToken(
-                roles = kratosSession!!.identity?.metadata_public?.parseRoles()!!,
-                scopes = kratosSession!!.identity?.metadata_public?.scope?.toSet()!!,
-                sources = kratosSession!!.identity?.metadata_public?.sources!!,
-                grantType = "session",
-                subject = kratosSession!!.identity?.id,
-                issuedAt = Instant.parse(kratosSession!!.issued_at),
-                expiresAt = Instant.parse(kratosSession!!.expires_at),
-                audience = kratosSession!!.identity?.metadata_public?.aud!!,
-                token = token,
-                issuer = kratosSession!!.authentication_methods?.first()?.provider,
-                type = "type",
-                clientId = "kratosSession",
-                username = kratosSession!!.identity?.metadata_public?.mp_login
-            )
-        }
-        else {
-            throw TokenValidationException("couldn't get kratos session")
-        }
+        DataRadarToken(
+            roles = kratosSession.identity.metadata_public.parseRoles(),
+            scopes = kratosSession.identity.metadata_public.scope.toSet(),
+            sources = kratosSession.identity.metadata_public.sources,
+            grantType = "session",
+            subject = kratosSession.identity.id,
+            issuedAt = kratosSession.issued_at,
+            expiresAt = kratosSession.expires_at,
+            audience = kratosSession.identity.metadata_public.aud,
+            token = token,
+            issuer = kratosSession.authentication_methods.first().provider,
+            type = "type",
+            clientId = "kratosSession",
+            username = kratosSession.identity.metadata_public.mp_login
+        )
     } catch (ex: Throwable) {
         throw ex
     }
 
+    object InstantSerializer : KSerializer<Instant> {
+        override val descriptor = PrimitiveSerialDescriptor("Instant", PrimitiveKind.STRING)
+
+        override fun deserialize(decoder: Decoder): Instant {
+            return Instant.parse(decoder.decodeString())
+        }
+
+        override fun serialize(encoder: kotlinx.serialization.encoding.Encoder, value: Instant) {
+            encoder.encodeString(value.toString())
+        }
+    }
     override fun toString(): String = "KratosTokenVerifier"
 
     companion object {
         private val logger = LoggerFactory.getLogger(KratosTokenVerifier::class.java)
 
         private fun Metadata.parseRoles(): Set<AuthorityReference> = buildSet {
-            if (!this@parseRoles.authorities.isNullOrEmpty()) {
-                for (roleValue in this@parseRoles.authorities!!) {
+            if (this@parseRoles.authorities.isNotEmpty()) {
+                for (roleValue in this@parseRoles.authorities) {
                     val authority = RoleAuthority.valueOfAuthorityOrNull(roleValue)
                     if (authority?.scope == RoleAuthority.Scope.GLOBAL) {
                         add(AuthorityReference(authority))
                     }
                 }
             }
-            if (!this@parseRoles.roles.isNullOrEmpty()) {
-                for (roleValue in this@parseRoles.roles!!) {
+            if (this@parseRoles.roles.isNotEmpty()) {
+                for (roleValue in this@parseRoles.roles) {
                     val role = RoleAuthority.valueOfAuthorityOrNull(roleValue)
                     if (role?.scope == RoleAuthority.Scope.GLOBAL) {
                         add(AuthorityReference(role))
@@ -109,63 +121,69 @@ class KratosTokenVerifier : TokenVerifier {
     }
 
     @Serializable
-    class AuthenticationMethod {
-        var method: String? = null
-        var aal: String? = null
-        var completed_at: String? = null
-        var provider: String? = null
-    }
+    data class AuthenticationMethod(
+        val method: String,
+        val aal: String,
+        @Serializable(with = InstantSerializer::class)
+        val completed_at: Instant,
+        val provider: String
+    )
 
     @Serializable
-    class Device {
-        var id: String? = null
-        var ip_address: String? = null
-        var user_agent: String? = null
-        var location: String? = null
-    }
+    class Device(
+        val id: String,
+        val ip_address: String,
+        val user_agent: String,
+        val location: String,
+    )
 
     @Serializable
-    class Identity {
-        var id: String? = null
-        var schema_id: String? = null
-        var schema_url: String? = null
-        var state: String? = null
-        var state_changed_at: String? = null
-        var traits: Traits? = null
-        var metadata_public: Metadata? = null
-        var created_at: String? = null
-        var updated_at: String? = null
-    }
-
-
-    @Serializable
-    class KratosDTO {
-        var id: String? = null
-        var active = false
-        var expires_at: String? = null
-        var authenticated_at: String? = null
-        var authenticator_assurance_level: String? = null
-        var authentication_methods: ArrayList<AuthenticationMethod>? = null
-        var issued_at: String? = null
-        var identity: Identity? = null
-        var devices: ArrayList<Device>? = null
-    }
+    class Identity (
+        val id: String,
+        val schema_id: String,
+        val schema_url: String,
+        val state: String,
+        @Serializable(with = InstantSerializer::class)
+        val state_changed_at: Instant,
+        val traits: Traits?,
+        val metadata_public: Metadata,
+        @Serializable(with = InstantSerializer::class)
+        val created_at: Instant,
+        @Serializable(with = InstantSerializer::class)
+        val updated_at: Instant,
+    )
 
 
     @Serializable
-    class Traits {
-        var name: String? = null
-        var email: String? = null
-        var mp_id = 0
-    }
+    class KratosDTO (
+        val id: String,
+        val active: Boolean,
+        @Serializable(with = InstantSerializer::class)
+        val expires_at: Instant,
+        @Serializable(with = InstantSerializer::class)
+        val authenticated_at: Instant,
+        val authenticator_assurance_level: String,
+        val authentication_methods: ArrayList<AuthenticationMethod>,
+        @Serializable(with = InstantSerializer::class)
+        val issued_at: Instant,
+        val identity: Identity,
+        val devices: ArrayList<Device>
+    )
+
 
     @Serializable
-    class Metadata {
-        var roles: ArrayList<String>? = null
-        var authorities: ArrayList<String>? = null
-        var scope: ArrayList<String>? = null
-        var sources: ArrayList<String>? = null
-        var aud: ArrayList<String>? = null
-        var mp_login: String? = ""
-    }
+    class Traits (
+        val name: String?,
+        val email: String
+    )
+
+    @Serializable
+    class Metadata (
+        val roles: ArrayList<String>,
+        val authorities: ArrayList<String>,
+        val scope: ArrayList<String>,
+        val sources: ArrayList<String>,
+        val aud: ArrayList<String>,
+        val mp_login: String?
+    )
 }
