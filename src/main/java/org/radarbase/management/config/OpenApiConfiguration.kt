bf6fbf46fb480b13f4d75b255fdecdf95556b346
Bastiaan <bastiaan@thehyve.nl>
2023-10-25 14:58:38 +0200

    Rename .java to .kt

--- src/main/java/org/radarbase/management/config/OpenApiConfiguration.kt
+++ src/main/java/org/radarbase/management/config/OpenApiConfiguration.kt
@@ -6,54 +6,41 @@
  *
  * See the file LICENSE in the root of this repository.
  */
-package org.radarbase.management.config
 
-import io.swagger.v3.oas.models.Components
-import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.info.License
-import io.swagger.v3.oas.models.security.OAuthFlow
-import io.swagger.v3.oas.models.security.OAuthFlows
-import io.swagger.v3.oas.models.security.SecurityScheme
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-import tech.jhipster.config.JHipsterConstants
+package org.radarbase.management.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import tech.jhipster.config.JHipsterConstants;
 
 @Configuration
 @Profile(JHipsterConstants.SPRING_PROFILE_API_DOCS)
-open class OpenApiConfiguration {
+public class OpenApiConfiguration {
     @Bean
-    open fun customOpenAPI(): OpenAPI {
-        return OpenAPI()
-            .components(
-                Components()
-                    .addSecuritySchemes(
-                        "oauth2Login", SecurityScheme()
-                            .type(SecurityScheme.Type.OAUTH2)
-                            .flows(
-                                OAuthFlows()
-                                    .authorizationCode(
-                                        OAuthFlow()
-                                            .authorizationUrl("/oauth/authorize")
-                                            .tokenUrl("/oauth/token")
-                                    )
-                                    .clientCredentials(
-                                        OAuthFlow()
-                                            .tokenUrl("/oauth/token")
-                                    )
-                            )
-                    )
-            )
-            .info(
-                Info()
-                    .title("ManagementPortal API")
-                    .description("ManagementPortal for RADAR-base")
-                    .license(
-                        License()
-                            .name("Apache 2.0")
-                            .url("https://radar-base.org")
-                    )
-            )
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("oauth2Login", new SecurityScheme()
+                                .type(SecurityScheme.Type.OAUTH2)
+                                .flows(new OAuthFlows()
+                                        .authorizationCode(new OAuthFlow()
+                                                .authorizationUrl("/oauth/authorize")
+                                                .tokenUrl("/oauth/token"))
+                                        .clientCredentials(new OAuthFlow()
+                                                .tokenUrl("/oauth/token")))))
+                .info(new Info()
+                        .title("ManagementPortal API")
+                        .description("ManagementPortal for RADAR-base")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://radar-base.org")));
     }
 }
--- src/main/java/org/radarbase/management/config/RadarTokenConfiguration.kt
+++ src/main/java/org/radarbase/management/config/RadarTokenConfiguration.kt
@@ -1,19 +1,30 @@
-package org.radarbase.management.config
+package org.radarbase.management.config;
 
-import org.radarbase.auth.token.RadarToken
-import org.radarbase.management.security.jwt.RadarTokenLoader
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Scope
-import org.springframework.context.annotation.ScopedProxyMode
-import javax.servlet.http.HttpServletRequest
+import org.radarbase.auth.token.RadarToken;
+import org.radarbase.management.security.jwt.RadarTokenLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.Nullable;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.context.annotation.ScopedProxyMode.TARGET_CLASS;
 
 @Configuration
-open class RadarTokenConfiguration @Autowired constructor(private val radarTokenLoader: RadarTokenLoader) {
-    @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class RadarTokenConfiguration {
+    private final RadarTokenLoader radarTokenLoader;
+
+    @Autowired
+    public RadarTokenConfiguration(RadarTokenLoader radarTokenLoader) {
+        this.radarTokenLoader = radarTokenLoader;
+    }
+
+    @Scope(value = "request", proxyMode = TARGET_CLASS)
     @Bean
-    open fun radarToken(request: HttpServletRequest?): RadarToken? {
-        return radarTokenLoader.loadToken(request!!)
+    @Nullable
+    public RadarToken radarToken(HttpServletRequest request) {
+        return radarTokenLoader.loadToken(request);
     }
 }
