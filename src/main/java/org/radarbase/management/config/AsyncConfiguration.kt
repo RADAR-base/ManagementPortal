ae6b161cdf262017a83c514d8390240e89b2c8c3
Bastiaan <bastiaan@thehyve.nl>
2023-10-25 14:57:06 +0200

    first conversions, tests passing

--- src/main/java/org/radarbase/management/config/AsyncConfiguration.kt
+++ src/main/java/org/radarbase/management/config/AsyncConfiguration.kt
@@ -1,43 +1,40 @@
-package org.radarbase.management.config;
+package org.radarbase.management.config
 
-import tech.jhipster.async.ExceptionHandlingAsyncTaskExecutor;
-import tech.jhipster.config.JHipsterProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.slf4j.LoggerFactory
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.AsyncConfigurer
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import tech.jhipster.async.ExceptionHandlingAsyncTaskExecutor
+import tech.jhipster.config.JHipsterProperties
 
 @Configuration
 @EnableAsync
 @EnableScheduling
-public class AsyncConfiguration implements AsyncConfigurer {
-
-    private static final Logger log = LoggerFactory.getLogger(AsyncConfiguration.class);
-
+open class AsyncConfiguration : AsyncConfigurer {
     @Autowired
-    private JHipsterProperties jHipsterProperties;
-
-    @Override
-    @Bean(name = "taskExecutor")
-    public ExceptionHandlingAsyncTaskExecutor getAsyncExecutor() {
-        log.debug("Creating Async Task Executor");
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(jHipsterProperties.getAsync().getCorePoolSize());
-        executor.setMaxPoolSize(jHipsterProperties.getAsync().getMaxPoolSize());
-        executor.setQueueCapacity(jHipsterProperties.getAsync().getQueueCapacity());
-        executor.setThreadNamePrefix("management-portal-Executor-");
-        return new ExceptionHandlingAsyncTaskExecutor(executor);
+    private val jHipsterProperties: JHipsterProperties? = null
+    @Bean(name = ["taskExecutor"])
+    override fun getAsyncExecutor(): ExceptionHandlingAsyncTaskExecutor {
+        log.debug("Creating Async Task Executor")
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = jHipsterProperties!!.async.corePoolSize
+        executor.maxPoolSize = jHipsterProperties.async.maxPoolSize
+        executor.queueCapacity = jHipsterProperties.async.queueCapacity
+        executor.setThreadNamePrefix("management-portal-Executor-")
+        return ExceptionHandlingAsyncTaskExecutor(executor)
     }
 
-    @Override
-    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new SimpleAsyncUncaughtExceptionHandler();
+    override fun getAsyncUncaughtExceptionHandler(): AsyncUncaughtExceptionHandler {
+        return SimpleAsyncUncaughtExceptionHandler()
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(AsyncConfiguration::class.java)
     }
 }
