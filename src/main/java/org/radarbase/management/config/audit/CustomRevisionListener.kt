261a4617cd2b6ef395c252207ee00707a36278c9
Bastiaan <bastiaan@thehyve.nl>
2023-10-25 15:03:02 +0200

    finished config folder

--- src/main/java/org/radarbase/management/config/audit/CustomRevisionListener.kt
+++ src/main/java/org/radarbase/management/config/audit/CustomRevisionListener.kt
@@ -1,22 +1,20 @@
-package org.radarbase.management.config.audit;
+package org.radarbase.management.config.audit
 
-import org.hibernate.envers.RevisionListener;
-import org.radarbase.management.security.Constants;
-import org.radarbase.management.domain.audit.CustomRevisionEntity;
-import org.radarbase.management.domain.support.AutowireHelper;
-import org.radarbase.management.security.SpringSecurityAuditorAware;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.envers.RevisionListener
+import org.radarbase.management.domain.audit.CustomRevisionEntity
+import org.radarbase.management.domain.support.AutowireHelper
+import org.radarbase.management.security.Constants
+import org.radarbase.management.security.SpringSecurityAuditorAware
+import org.springframework.beans.factory.annotation.Autowired
 
-public class CustomRevisionListener implements RevisionListener {
 
+class CustomRevisionListener : RevisionListener {
     @Autowired
-    private SpringSecurityAuditorAware springSecurityAuditorAware;
-
-    @Override
-    public void newRevision(Object revisionEntity) {
-        AutowireHelper.autowire(this, springSecurityAuditorAware);
-        CustomRevisionEntity entity = (CustomRevisionEntity) revisionEntity;
-        entity.setAuditor(springSecurityAuditorAware.getCurrentAuditor()
-                .orElse(Constants.SYSTEM_ACCOUNT));
+    private val springSecurityAuditorAware: SpringSecurityAuditorAware? = null
+    override fun newRevision(revisionEntity: Any) {
+        AutowireHelper.autowire(this, springSecurityAuditorAware)
+        val entity = revisionEntity as CustomRevisionEntity
+        entity.auditor = springSecurityAuditorAware!!.currentAuditor
+            .orElse(Constants.SYSTEM_ACCOUNT)
     }
 }
