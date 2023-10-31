5e1220225211e39b687fc0dfe140ab3ca2719b07
Bastiaan <bastiaan@thehyve.nl>
2023-10-31 10:13:27 +0100

    change the renamed boolean properties back so hibernate plays nice

--- src/main/java/org/radarbase/management/service/dto/SourceDataDTO.kt
+++ src/main/java/org/radarbase/management/service/dto/SourceDataDTO.kt
@@ -34,14 +34,14 @@ class SourceDataDTO : Serializable {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     var sourceType: MinimalSourceTypeDTO? = null
 
-    override fun equals(o: Any?): Boolean {
-        if (this === o) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
             return true
         }
-        if (o == null || javaClass != o.javaClass) {
+        if (other == null || javaClass != other.javaClass) {
             return false
         }
-        val sourceDataDto = o as SourceDataDTO
+        val sourceDataDto = other as SourceDataDTO
         return if (sourceDataDto.id == null || id == null) {
             false
         } else id == sourceDataDto.id
