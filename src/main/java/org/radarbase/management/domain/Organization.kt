c9fefe83eebd91f8caf764c697303929ffafda48
Bastiaan <bastiaan@thehyve.nl>
2023-11-01 11:30:12 +0100

    misc fixes

--- src/main/java/org/radarbase/management/domain/Organization.kt
+++ src/main/java/org/radarbase/management/domain/Organization.kt
@@ -49,14 +49,14 @@ class Organization : AbstractEntity() {
     @JvmField
     @OneToMany(mappedBy = "organization")
     var projects: List<Project>? = null
-    override fun equals(o: Any?): Boolean {
-        if (this === o) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
             return true
         }
-        if (o == null || javaClass != o.javaClass) {
+        if (other == null || javaClass != other.javaClass) {
             return false
         }
-        val org = o as Organization
+        val org = other as Organization
         return if (org.id == null || id == null) {
             false
         } else id == org.id
