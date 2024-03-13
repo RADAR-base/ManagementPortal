5106366100a21fc063e1aca4ef3c811ba7e66da3
Bastiaan <bastiaan@thehyve.nl>
2024-03-13 14:27:40 +0100

    remaining issues will not be fixed

--- src/main/java/org/radarbase/management/config/DatabaseConfiguration.java
+++ src/main/java/org/radarbase/management/config/DatabaseConfiguration.java
@@ -2,14 +2,12 @@ package org.radarbase.management.config;
 
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import liquibase.integration.spring.SpringLiquibase;
-import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
@@ -19,7 +17,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tech.jhipster.config.JHipsterConstants;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "org.radarbase.management.repository",
