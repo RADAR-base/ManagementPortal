92c77015d4b5b9ca9a350d9c766b5464189f2676
jindrichgorner <jindrich.gorner@manchester.ac.uk>
2024-05-24 13:28:03 +0100

    fitbit added back
    -fitbit added back
    - removed some unused imports

--- src/main/java/org/radarbase/management/repository/ConnectDataLogRepository.java
+++ src/main/java/org/radarbase/management/repository/ConnectDataLogRepository.java
@@ -1,16 +1,11 @@
 package org.radarbase.management.repository;
 
 
-import org.radarbase.management.domain.Authority;
 import org.radarbase.management.domain.ConnectDataLog;
-import org.radarbase.management.domain.enumeration.DataGroupingType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
 import org.springframework.data.repository.history.RevisionRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.time.Instant;
 import java.util.Optional;
 
 @RepositoryDefinition(domainClass = ConnectDataLog.class, idClass = String.class)
