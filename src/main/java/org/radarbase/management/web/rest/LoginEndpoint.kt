7fde553e986d97966deb6f9b144ff0886262b950
Pauline <mpgxvii@gmail.com>
2024-07-04 12:32:41 +0100

    Fix import for RequestMapping

--- src/main/java/org/radarbase/management/web/rest/LoginEndpoint.kt
+++ src/main/java/org/radarbase/management/web/rest/LoginEndpoint.kt
@@ -4,6 +4,7 @@ import org.radarbase.management.config.ManagementPortalProperties
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.view.RedirectView
 
