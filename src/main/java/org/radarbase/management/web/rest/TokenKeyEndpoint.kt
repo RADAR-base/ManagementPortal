1882851fc1e6b07594664de3615873263e1249ec
Pauline <mpgxvii@gmail.com>
2024-08-22 15:39:51 +0100

    Remove OAuth2ServerConfiguration and use single SecurityConfig for auth

--- src/main/java/org/radarbase/management/web/rest/TokenKeyEndpoint.kt
+++ src/main/java/org/radarbase/management/web/rest/TokenKeyEndpoint.kt
@@ -2,7 +2,6 @@ package org.radarbase.management.web.rest
 
 import io.micrometer.core.annotation.Timed
 import org.radarbase.auth.jwks.JsonWebKeySet
-import org.radarbase.management.security.jwt.ManagementPortalOauthKeyStoreHandler
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,11 +9,10 @@ import org.springframework.web.bind.annotation.RestController
 
 //@RestController
 class TokenKeyEndpoint @Autowired constructor(
-    private val keyStoreHandler: ManagementPortalOauthKeyStoreHandler
 ) {
     @get:Timed
     @get:GetMapping("/oauth/token_key")
-    val key: JsonWebKeySet
+    val key: JsonWebKeySet?
         /**
          * Get the verification key for the token signatures. The principal has to
          * be provided only if the key is secret
@@ -23,7 +21,7 @@ class TokenKeyEndpoint @Autowired constructor(
          */
         get() {
             logger.debug("Requesting verifier public keys...")
-            return keyStoreHandler.loadJwks()
+            return null
         }
 
     companion object {
