7f964b7a34e061e8e12d5f18a108538a0fe5f476
Gwen Zhou <jiawen.zhou@manchester.ac.uk>
2025-03-05 16:51:06 +0000

    the query builder with good styles

--- src/main/webapp/app/shared/queries/addQuery.component.ts
+++ src/main/webapp/app/shared/queries/addQuery.component.ts
@@ -1,13 +1,203 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+    QueryBuilderClassNames,
+    QueryBuilderConfig,
+} from 'ngx-angular-query-builder';
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
 
 @Component({
     selector: 'jhi-queries',
     templateUrl: './addQuery.component.html',
+    styleUrls: ['../../../content/scss/queries.scss'],
 })
-export class AddQueryComponent implements OnInit, OnDestroy {
-    constructor() {}
+export class AddQueryComponent {
+    @ViewChild('queryForm', { static: true })
+    queryBuilderFormGroup: NgForm;
 
-    ngOnInit() {}
+    public queryCtrl: FormControl;
 
-    ngOnDestroy() {}
+    public bootstrapClassNames: QueryBuilderClassNames = {
+        removeIcon: 'fa fa-minus',
+        addIcon: 'fa fa-plus',
+        arrowIcon: 'fa fa-chevron-right px-2',
+        button: 'btn',
+        buttonGroup: 'btn-group',
+        rightAlign: 'order-12 ml-auto',
+        switchRow: 'd-flex px-2',
+        switchGroup: 'd-flex align-items-center',
+        switchRadio: 'custom-control-input',
+        switchLabel: 'custom-control-label',
+        switchControl: 'custom-control custom-radio custom-control-inline',
+        row: 'row p-2 m-1',
+        rule: 'border',
+        ruleSet: 'border',
+        invalidRuleSet: 'alert alert-danger',
+        emptyWarning: 'text-danger mx-auto',
+        operatorControl: 'form-control',
+        operatorControlSize: 'col-auto pr-0',
+        fieldControl: 'form-control',
+        fieldControlSize: 'col-auto pr-0',
+        entityControl: 'form-control',
+        entityControlSize: 'col-auto pr-0',
+        inputControl: 'form-control',
+        inputControlSize: 'col-auto',
+    };
+
+    public query = {
+        condition: 'and',
+        rules: [
+            { field: 'age', operator: '<=', entity: 'physical' },
+            {
+                field: 'birthday',
+                operator: '=',
+                value: new Date(),
+                entity: 'nonphysical',
+            },
+            {
+                condition: 'or',
+                rules: [
+                    { field: 'gender', operator: '=', entity: 'physical' },
+                    {
+                        field: 'occupation',
+                        operator: 'in',
+                        entity: 'nonphysical',
+                    },
+                    {
+                        field: 'school',
+                        operator: 'is null',
+                        entity: 'nonphysical',
+                    },
+                    { field: 'notes', operator: '=', entity: 'nonphysical' },
+                ],
+            },
+        ],
+    };
+
+    public entityConfig: QueryBuilderConfig = {
+        entities: {
+            physical: { name: 'Physical Attributes' },
+            nonphysical: { name: 'Nonphysical Attributes' },
+        },
+        fields: {
+            age: { name: 'Age', type: 'number', entity: 'physical' },
+            gender: {
+                name: 'Gender',
+                entity: 'physical',
+                type: 'category',
+                options: [
+                    { name: 'Male', value: 'm' },
+                    { name: 'Female', value: 'f' },
+                ],
+            },
+            name: { name: 'Name', type: 'string', entity: 'nonphysical' },
+            notes: {
+                name: 'Notes',
+                type: 'textarea',
+                operators: ['=', '!='],
+                entity: 'nonphysical',
+            },
+            educated: {
+                name: 'College Degree?',
+                type: 'boolean',
+                entity: 'nonphysical',
+            },
+            birthday: {
+                name: 'Birthday',
+                type: 'date',
+                operators: ['=', '<=', '>'],
+                defaultValue: () => new Date(),
+                entity: 'nonphysical',
+            },
+            school: {
+                name: 'School',
+                type: 'string',
+                nullable: true,
+                entity: 'nonphysical',
+            },
+            occupation: {
+                name: 'Occupation',
+                entity: 'nonphysical',
+                type: 'category',
+                options: [
+                    { name: 'Student', value: 'student' },
+                    { name: 'Teacher', value: 'teacher' },
+                    { name: 'Unemployed', value: 'unemployed' },
+                    { name: 'Scientist', value: 'scientist' },
+                ],
+            },
+        },
+    };
+
+    public config: QueryBuilderConfig = {
+        fields: {
+            age: { name: 'Age', type: 'number' },
+            gender: {
+                name: 'Gender',
+                type: 'category',
+                options: [
+                    { name: 'Male', value: 'm' },
+                    { name: 'Female', value: 'f' },
+                ],
+            },
+            name: { name: 'Name', type: 'string' },
+            notes: { name: 'Notes', type: 'textarea', operators: ['=', '!='] },
+            educated: { name: 'College Degree?', type: 'boolean' },
+            birthday: {
+                name: 'Birthday',
+                type: 'date',
+                operators: ['=', '<=', '>'],
+                defaultValue: () => new Date(),
+            },
+            school: { name: 'School', type: 'string', nullable: true },
+            occupation: {
+                name: 'Occupation',
+                type: 'category',
+                options: [
+                    { name: 'Student', value: 'student' },
+                    { name: 'Teacher', value: 'teacher' },
+                    { name: 'Unemployed', value: 'unemployed' },
+                    { name: 'Scientist', value: 'scientist' },
+                ],
+            },
+        },
+    };
+
+    public currentConfig: QueryBuilderConfig;
+    public allowRuleset: boolean = true;
+    public allowCollapse: boolean;
+    public persistValueOnFieldChange: boolean = false;
+
+    constructor(private formBuilder: FormBuilder) {
+        this.queryCtrl = this.formBuilder.control(this.query);
+        this.currentConfig = this.config;
+    }
+
+    ngAfterViewInit() {
+        //console.log(this.queryBuilderFormGroup)
+        setTimeout(() => this.queryBuilderFormGroup.form.markAllAsTouched(), 0);
+        //this.queryBuilderFormGroup.form.markAllAsTouched();
+    }
+
+    switchModes(event: Event) {
+        this.currentConfig = (<HTMLInputElement>event.target).checked
+            ? this.entityConfig
+            : this.config;
+    }
+
+    changeDisabled(event: Event) {
+        (<HTMLInputElement>event.target).checked
+            ? this.queryCtrl.disable()
+            : this.queryCtrl.enable();
+    }
+
+    private _counter = 0;
+    formRuleWeakMap = new WeakMap();
+
+    getUniqueName(prefix: string, rule: any) {
+        if (!this.formRuleWeakMap.has(rule)) {
+            this.formRuleWeakMap.set(rule, `${prefix}-${++this._counter}`);
+        }
+
+        return this.formRuleWeakMap.get(rule);
+    }
 }
