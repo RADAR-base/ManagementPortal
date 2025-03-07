e8d077eb450aa7d7142311837ce6f737b95befde
pvannierop <pim@thehyve.nl>
2025-03-07 08:53:33 +0100

    Profile dependent email sending in UserResource

--- src/main/java/org/radarbase/management/web/rest/UserResource.kt
+++ src/main/java/org/radarbase/management/web/rest/UserResource.kt
@@ -23,6 +23,7 @@ import org.radarbase.management.web.rest.util.PaginationUtil
 import org.radarbase.management.web.rest.vm.ManagedUserVM
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
@@ -73,11 +74,15 @@ import java.util.*
 @RequestMapping("/api")
 class UserResource(
     @Autowired private val userRepository: UserRepository,
+    @Autowired private val mailService: MailService,
     @Autowired private val userService: UserService,
     @Autowired private val subjectRepository: SubjectRepository,
     @Autowired private val managementPortalProperties: ManagementPortalProperties,
     @Autowired private val authService: AuthService
 ) {
+
+    @Value("\${spring.profiles.active:}")
+    lateinit var activeSpringProfiles: String
 
     /**
      * POST  /users  : Creates a new user.
@@ -117,10 +122,14 @@ class UserResource(
                 )
             ).body(null)
         } else {
-            val newUser: User;
-            newUser = userService.createUser(managedUserVm)
-            userService.sendActivationEmail(newUser)
-
+            val newUser: User = userService.createUser(managedUserVm)
+            if (activeSpringProfiles.contains("legacy-login"))
+                mailService.sendCreationEmail(
+                    newUser, managementPortalProperties.common.activationKeyTimeoutInSeconds.toLong()
+                )
+            else {
+                userService.sendActivationEmail(newUser)
+            }
             ResponseEntity.created(ResourceUriService.getUri(newUser)).headers(
                 HeaderUtil.createAlert(
                     "userManagement.created", newUser.login
