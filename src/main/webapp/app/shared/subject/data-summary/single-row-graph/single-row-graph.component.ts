edb63c0e836d9f43fa5b94e1f677c091732a7ca9
jindrichgorner <jindrich.gorner@manchester.ac.uk>
2025-03-12 15:33:42 +0000

    fixed single bar chart colour

--- src/main/webapp/app/shared/subject/data-summary/single-row-graph/single-row-graph.component.ts
+++ src/main/webapp/app/shared/subject/data-summary/single-row-graph/single-row-graph.component.ts
@@ -29,9 +29,18 @@ export class SingleRowGraphComponent implements OnInit {
 
     @Input() css: string = '';
 
+    @Input() applyCustomColour = false;
+
     ngAfterViewInit() {
         //@ts-ignore
-        // const ctx = document.getElementById(this.chartId).getContext('2d');
+        // const ctx = document.getElementById(this.charxtId).getContext('2d');
+
+        if (this.applyCustomColour) {
+            this.chart.data.datasets[0]['backgroundColor'] = [
+                this.color.replace('0.9', '0.6'),
+            ];
+        }
+
         this.chartGraph = new Chart(this.chartId, this.chart);
     }
     chartGraph: any = {};
