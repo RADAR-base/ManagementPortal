e29ecd875338fb0658680157c28fe1dd0f8e2b58
Gwen Zhou <jiawen.zhou@manchester.ac.uk>
2025-03-18 16:49:40 +0000

    get queries from server

--- src/main/webapp/app/shared/queries/queries.component.ts
+++ src/main/webapp/app/shared/queries/queries.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-queries',
-    templateUrl: './queries.component.html'
+    templateUrl: './queries.component.html',
 })
+export class QueriesComponent implements OnInit, OnDestroy {
+    constructor(private httpclient: HttpClient) {}
+    public queryList: any;
 
-export class QueriesComponent implements OnInit, OnDestroy{
+    private baseUrl = 'api/query-builder';
 
-    constructor(
-
-    ) {
-    }
-   
     ngOnInit() {
+        this.httpclient.get(this.baseUrl + '/queries').subscribe((result) => {
+            this.queryList = result;
+            console.log(result);
+        });
     }
 
-    ngOnDestroy() {
-    }
-
+    ngOnDestroy() {}
 }
