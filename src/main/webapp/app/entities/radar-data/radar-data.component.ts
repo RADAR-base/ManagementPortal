9348c6db96f25648ce18de69aa7f55f850f2a40b
Gwen Zhou <jiawen.zhou@manchester.ac.uk>
2025-05-27 17:26:02 +0100

    display datalogs

--- src/main/webapp/app/entities/radar-data/radar-data.component.ts
+++ src/main/webapp/app/entities/radar-data/radar-data.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable, combineLatest, forkJoin, of } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
-import { catchError } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import {
     ProjectService,
     OrganizationService,
@@ -10,12 +9,17 @@ import {
 } from 'app/shared';
 import { SubjectService } from 'app/shared/subject';
 import { Subject as Participant } from 'app/shared/subject';
+import { HttpResponse } from '@angular/common/http';
+
+interface SubjectWithDataLogs extends Participant {
+    dataLogs?: { [type: string]: string }; // e.g., {"PASSIVE_ANDROID_PHONE": "2024-05-01", ...}
+}
 
 interface GroupedSubjects {
     organization: Organization;
     projects: {
         project: Project;
-        subjects: Participant[];
+        subjects: SubjectWithDataLogs[];
     }[];
 }
 
@@ -40,52 +44,80 @@ export class RadarDataComponent implements OnInit {
     ngOnInit(): void {
         this.groupedSubjects$ = this.organizationService.findAll().pipe(
             switchMap((orgs) => {
-                if (!orgs.length) return of([]);
-                return combineLatest(
-                    orgs.map((org) =>
-                        this.projectService.findByOrganization(org.id).pipe(
-                            switchMap((projects) => {
-                                if (!projects.length)
-                                    return of({
-                                        organization: org,
-                                        projects: [],
-                                    });
-                                return combineLatest(
-                                    projects.map((p) =>
-                                        this.subjectService
-                                            .findAllByProject(
-                                                p.projectName,
-                                                {},
-                                                { size: 1000 }
-                                            )
-                                            .pipe(
-                                                map((res) => ({
-                                                    project: p,
-                                                    subjects: res.body || [],
-                                                })),
-                                                catchError(() =>
-                                                    of({
-                                                        project: p,
-                                                        subjects: [],
-                                                    })
-                                                )
-                                            )
+                const orgObservables = orgs.map((org) =>
+                    this.projectService.findByOrganization(org.id).pipe(
+                        switchMap((projects) => {
+                            const projectObservables = projects.map((project) =>
+                                this.subjectService
+                                    .findAllByProject(
+                                        project.projectName,
+                                        {},
+                                        { size: 1000 }
                                     )
-                                ).pipe(
-                                    map((projectsWithSubjects) => ({
-                                        organization: org,
-                                        projects: projectsWithSubjects,
-                                    }))
-                                );
-                            }),
-                            catchError(() =>
-                                of({ organization: org, projects: [] })
-                            )
-                        )
+                                    .pipe(
+                                        map((res) => res.body || []),
+                                        switchMap(
+                                            (
+                                                subjects: SubjectWithDataLogs[]
+                                            ) => {
+                                                if (!subjects.length)
+                                                    return of({
+                                                        project,
+                                                        subjects: [],
+                                                    });
+
+                                                return this.fetchDataLogsForSubjects(
+                                                    subjects
+                                                ).pipe(
+                                                    map((subjectsWithLogs) => ({
+                                                        project,
+                                                        subjects:
+                                                            subjectsWithLogs,
+                                                    }))
+                                                );
+                                            }
+                                        )
+                                    )
+                            );
+
+                            return projectObservables.length
+                                ? forkJoin(projectObservables).pipe(
+                                      map((projectsWithSubjects) => ({
+                                          organization: org,
+                                          projects: projectsWithSubjects,
+                                      }))
+                                  )
+                                : of({ organization: org, projects: [] });
+                        })
                     )
                 );
-            }),
-            catchError(() => of([]))
+
+                return combineLatest(orgObservables);
+            })
+        );
+    }
+
+    fetchDataLogsForSubjects(
+        subjects: SubjectWithDataLogs[]
+    ): Observable<SubjectWithDataLogs[]> {
+        return forkJoin(
+            subjects.map((subject) =>
+                this.subjectService.findDataLogs(subject.login).pipe(
+                    map((response: HttpResponse<any>) => {
+                        const logs = (response.body || []).reduce(
+                            (acc, cur) => {
+                                acc[cur.groupingType] = new Date(
+                                    cur.time
+                                ).toDateString();
+                                return acc;
+                            },
+                            {} as { [type: string]: string }
+                        );
+                        subject.dataLogs = logs;
+                        return subject;
+                    })
+                )
+            )
         );
     }
 }
