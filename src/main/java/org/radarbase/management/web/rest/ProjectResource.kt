8e0f453a3cf2bdf4e6276497a52f292e38f1ca45
ewelinagr <ewelina@thehyve.nl>
2025-08-22 10:01:25 +0200

    fix: proper permission check instead of checking the scope.

--- src/main/java/org/radarbase/management/web/rest/ProjectResource.kt
+++ src/main/java/org/radarbase/management/web/rest/ProjectResource.kt
@@ -184,7 +184,7 @@ class ProjectResource(
         @RequestParam(name = "minimized", required = false, defaultValue = "false") minimized: Boolean
     ): ResponseEntity<*> {
         log.debug("REST request to get Projects")
-        authService.checkScope(Permission.PROJECT_READ)
+        authService.checkPermission(Permission.PROJECT_READ)
         val page = projectService.findAll(minimized, pageable)
         val headers = PaginationUtil
             .generatePaginationHttpHeaders(page, "/api/projects")
